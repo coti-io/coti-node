05259f5d63daed68ee5e2253479c74a8903a2dbf
cotitech <eli@coti.io>
2018-06-07 20:38:36 +0300

    first commit

---
+++ src/main/java/io/coti/cotinode/CotiNodeApplication.java
@@ -0,0 +1,12 @@
+package io.coti.cotinode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotiNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CotiNodeApplication.class, args);
+    }
+}
---
+++ src/test/java/io/coti/cotinode/CotiNodeApplicationTests.java
@@ -0,0 +1,16 @@
+package io.coti.cotinode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CotiNodeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
