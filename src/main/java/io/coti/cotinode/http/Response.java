d6e156e52ed3f97b6029282b2956339f4d45b833
Barak Yaari <barakyaari@gmail.com>
2018-06-18 19:09:00 +0300

    Fixed Order in HttpMessages

--- src/main/java/io/coti/cotinode/http/Response.java
+++ src/main/java/io/coti/cotinode/http/Response.java
@@ -10,10 +10,10 @@ public abstract class Response implements IResponse {
     public static final String MESSAGE_SUCCESS = "Operation Successful";
     public static final String ERROR_MESSAGE_UNAUTHORIZED = "Unauthorized";
     public static final String ERROR_MESSAGE_INCORRECT_ARGUMENTS = "Incorrect Arguments Received";
+    public static final String ERROR_TYPE_INCORRECT_ARGUMENTS = "Incorrect Arguments Error Type";
 
-
-    public String message;
     public String status;
+    public String message;
     public String errorType;
 
     public Response(String status, String message) {
