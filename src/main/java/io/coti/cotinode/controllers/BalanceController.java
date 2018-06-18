ab3777323dad244196af6f75c5e5e69eef6ac5ce
Barak Yaari <barakyaari@gmail.com>
2018-06-18 19:54:01 +0300

    Fixed message Type

--- src/main/java/io/coti/cotinode/controllers/BalanceController.java
+++ src/main/java/io/coti/cotinode/controllers/BalanceController.java
@@ -2,17 +2,16 @@ package io.coti.cotinode.controllers;
 
 import io.coti.cotinode.http.GetBalancesRequest;
 import io.coti.cotinode.http.GetBalancesResponse;
-import io.coti.cotinode.http.Response;
 import io.coti.cotinode.service.interfaces.IBalanceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import static io.coti.cotinode.http.Response.ERROR_MESSAGE_INCORRECT_ARGUMENTS;
 import static io.coti.cotinode.http.Response.STATUS_ERROR;
 
 @Slf4j
@@ -29,7 +28,7 @@ public class BalanceController {
         if (getBalancesRequest == null || getBalancesRequest.addresses == null) {
             return new GetBalancesResponse(
                     STATUS_ERROR,
-                    "Incorrect message arguments"
+                    ERROR_MESSAGE_INCORRECT_ARGUMENTS
             );
         }
         log.info(getBalancesRequest.toString());
--- src/main/java/io/coti/cotinode/http/GetBalancesRequest.java
+++ src/main/java/io/coti/cotinode/http/GetBalancesRequest.java
@@ -3,9 +3,11 @@ package io.coti.cotinode.http;
 import io.coti.cotinode.data.Hash;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Data
 public class GetBalancesRequest extends Request {
+    @NotBlank(message = "Addresses must not be null!")
     public List<Hash> addresses;
 }
