3952755a705a0d3f6547ded596ba11920ff936ae
Barak Yaari <barakyaari@gmail.com>
2018-06-26 11:17:49 +0300

    GenesisTransactions

--- src/main/java/io/coti/cotinode/service/ZeroSpendService.java
+++ src/main/java/io/coti/cotinode/service/ZeroSpendService.java
@@ -5,10 +5,17 @@ import io.coti.cotinode.data.TransactionData;
 import io.coti.cotinode.service.interfaces.IZeroSpendService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ZeroSpendService implements IZeroSpendService {
     @Override
     public TransactionData getZeroSpendTransaction(double trustScore){
         return new TransactionData(new Hash("AAAA"));
+    }
+
+    @Override
+    public List<TransactionData> getGenesisTransactions() {
+        return null;
     }
 }
