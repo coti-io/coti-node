b7117d3641b2bda3ea7ccbfa6e11eac89d813993
Barak Yaari <barakyaari@gmail.com>
2018-06-28 12:16:40 +0300

    Balances service

--- src/main/java/io/coti/cotinode/CotiNodeApplication.java
+++ src/main/java/io/coti/cotinode/CotiNodeApplication.java
@@ -10,6 +10,5 @@ public class CotiNodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CotiNodeApplication.class, args);
-
     }
 }
--- src/main/java/io/coti/cotinode/http/GetBalancesResponse.java
+++ src/main/java/io/coti/cotinode/http/GetBalancesResponse.java
@@ -4,7 +4,6 @@ import io.coti.cotinode.data.Hash;
 import lombok.Data;
 
 import java.util.AbstractMap;
-import java.util.Arrays;
 import java.util.List;
 
 @Data
@@ -14,7 +13,5 @@ public class GetBalancesResponse extends Response {
 
     public GetBalancesResponse() {
         super();
-        amounts = Arrays.asList(
-                new AbstractMap.SimpleEntry<>(new Hash("ABCD"), 21.2));
     }
 }
