dc0bfb2fd8cb515f30e52ea3525602391bd7f86f
Barak Yaari <barakyaari@gmail.com>
2018-08-07 19:23:03 +0300

    Fixed web config

--- trustscore/src/main/java/io/coti/trustscore/config/WebConfig.java
+++ trustscore/src/main/java/io/coti/trustscore/config/WebConfig.java
@@ -1,4 +1,16 @@
 package io.coti.trustscore.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+                .allowedHeaders("*");
+    }
 }
--- trustscore/src/main/java/io/coti/trustscore/config/WebConfigurerAdapter.java
+++ trustscore/src/main/java/io/coti/trustscore/config/WebConfigurerAdapter.java
@@ -1,4 +1,29 @@
 package io.coti.trustscore.config;
 
-public class WebConfigurerAdapter {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and()
+                .csrf().disable().authorizeRequests().anyRequest().permitAll();
+    }
+
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    public void corsHeaders(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
+    }
 }
