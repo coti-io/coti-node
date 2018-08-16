5e27b7c8053c8bdaeed780739b736c49f2de3347
cotitech <eli@coti.io>
2018-08-16 15:06:55 +0300

    getAddressTransactions removed from TrustScore TransactionService

--- trustscore/src/main/java/io/coti/trustscore/services/TransactionService.java
+++ trustscore/src/main/java/io/coti/trustscore/services/TransactionService.java
@@ -1,26 +1,14 @@
 package io.coti.trustscore.services;
 
 import io.coti.common.crypto.TransactionCrypto;
-import io.coti.common.data.AddressTransactionsHistory;
-import io.coti.common.data.Hash;
 import io.coti.common.data.TransactionData;
-import io.coti.common.http.BaseResponse;
-import io.coti.common.http.GetAddressTransactionHistory;
 import io.coti.common.http.data.TransactionStatus;
-import io.coti.common.model.AddressesTransactionsHistory;
-import io.coti.common.model.DbItem;
-import io.coti.common.model.Transactions;
 import io.coti.common.services.LiveView.WebSocketSender;
 import io.coti.common.services.interfaces.ITransactionHelper;
 import io.coti.common.services.interfaces.IValidationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Vector;
 
 @Slf4j
 @Service
@@ -32,27 +20,8 @@ public class TransactionService {
     @Autowired
     private IValidationService validationService;
     @Autowired
-    private AddressesTransactionsHistory addressesTransactionsHistory;
-    @Autowired
-    private Transactions transactions;
-    @Autowired
     private WebSocketSender webSocketSender;
 
-    public ResponseEntity<BaseResponse> getAddressTransactions(Hash addressHash) {
-        List<TransactionData> transactionsDataList = new Vector<>();
-        DbItem<AddressTransactionsHistory> dbAddress = addressesTransactionsHistory.getByHashItem(addressHash);
-
-        if (!dbAddress.isExists)
-            return ResponseEntity.status(HttpStatus.OK).body(new GetAddressTransactionHistory(transactionsDataList));
-
-        AddressTransactionsHistory history = dbAddress.item;
-        for (Hash transactionHash : history.getTransactionsHistory()) {
-            TransactionData transactionData = transactions.getByHash(transactionHash);
-            transactionsDataList.add(transactionData);
-
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(new GetAddressTransactionHistory(transactionsDataList));
-    }
 
     public void handlePropagatedTransaction(TransactionData transactionData) {
         log.info("Propagated Transaction received: {}", transactionData.getHash().toHexString());
@@ -66,7 +35,7 @@ public class TransactionService {
             log.info("Data Integrity validation failed");
             return;
         }
-        if(!transactionHelper.checkBalancesAndAddToPreBalance(transactionData)){
+        if (!transactionHelper.checkBalancesAndAddToPreBalance(transactionData)) {
             log.info("Balance check failed!");
             return;
         }
