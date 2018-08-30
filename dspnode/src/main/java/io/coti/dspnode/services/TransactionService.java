5766022db420a5d7312f888147d5ab1175e49e87
Yohai Agami <vbyohai@gmail.com>
2018-08-30 11:43:42 +0300

    remove validation of pot from propagated

--- dspnode/src/main/java/io/coti/dspnode/services/TransactionService.java
+++ dspnode/src/main/java/io/coti/dspnode/services/TransactionService.java
@@ -50,7 +50,6 @@ public class TransactionService extends BaseNodeTransactionService {
         }
         if (!transactionHelper.validateTransaction(transactionData) ||
                 !transactionCrypto.verifySignature(transactionData) ||
-                !validationService.validatePot(transactionData) ||
                 !transactionHelper.checkBalancesAndAddToPreBalance(transactionData)) {
             log.info("Invalid Transaction Received!");
             return "Invalid Transaction Received: " + transactionData.getHash();
