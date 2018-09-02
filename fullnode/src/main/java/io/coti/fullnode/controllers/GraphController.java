e066d71ef8c724464bff54694abcee12cf2feffd
ben-coti <ben@coti.io>
2018-09-02 18:46:49 +0300

    new api for coti live

---
+++ fullnode/src/main/java/io/coti/fullnode/controllers/GraphController.java
@@ -0,0 +1,25 @@
+package io.coti.fullnode.controllers;
+
+import io.coti.basenode.data.GraphData;
+import io.coti.basenode.services.LiveView.LiveViewService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RequestMapping("/fullgraph")
+@RestController
+@Slf4j
+public class GraphController {
+
+    @Autowired
+    private LiveViewService liveViewService;
+
+    @RequestMapping(method = GET)
+    public GraphData getFullGraph(){
+        log.info("graph : {}",liveViewService.getFullGraph());
+        return liveViewService.getFullGraph();
+    }
+}
