f6371cc3d00a444a586025c6ae129c321d5f1509
Yohai Agami <vbyohai@gmail.com>
2018-09-02 15:30:50 +0300

    project compile

--- basenode/src/main/java/io/coti/basenode/services/interfaces/IZeroSpendService.java
+++ basenode/src/main/java/io/coti/basenode/services/interfaces/IZeroSpendService.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface IZeroSpendService {
 
     List<TransactionData> getGenesisTransactions();
+    TransactionData getZeroSpendTransaction(double trustScore);
 }
--- tools/src/main/java/io/coti/tools/InitialDBCreator.java
+++ tools/src/main/java/io/coti/tools/InitialDBCreator.java
@@ -6,7 +6,6 @@ import io.coti.basenode.database.RocksDBConnector;
 import io.coti.basenode.model.TransactionIndexes;
 import io.coti.basenode.model.Transactions;
 import io.coti.basenode.services.TransactionIndexService;
-import io.coti.basenode.services.ZeroSpendService;
 
 import java.io.File;
 
@@ -16,20 +15,13 @@ public class InitialDBCreator {
         deleteInitialDatabaseFolder();
         RocksDBConnector connector = new RocksDBConnector();
         connector.init("initialDatabase");
-        ZeroSpendService zeroSpendService = new ZeroSpendService();
+
         Transactions transactions = new Transactions();
         transactions.init();
         transactions.databaseConnector = connector;
         TransactionIndexes transactionIndexes = new TransactionIndexes();
         transactionIndexes.init();
         transactionIndexes.databaseConnector = connector;
-
-        byte[] accumulatedHash = "GENESIS".getBytes();
-        for (TransactionData transactionData : zeroSpendService.getGenesisTransactions()) {
-            transactions.put(transactionData);
-            accumulatedHash = TransactionIndexService.getAccumulatedHash(accumulatedHash, transactionData.getHash(), transactionData.getDspConsensusResult().getIndex());
-            transactionIndexes.put(new TransactionIndexData(transactionData.getHash(), transactionData.getDspConsensusResult().getIndex(), accumulatedHash));
-        }
     }
 
     private static void deleteInitialDatabaseFolder() {
