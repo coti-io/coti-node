49c6bc0ce1e21b45cfe6f0e476b6238c5c4cc03d
Yohai Agami <vbyohai@gmail.com>
2018-09-26 14:52:37 +0300

    adding history buckets

--- trustscore/src/main/java/io/coti/trustscore/data/HistoryBucketEvents.java
+++ trustscore/src/main/java/io/coti/trustscore/data/HistoryBucketEvents.java
@@ -1,12 +1,23 @@
 package io.coti.trustscore.data;
 
 import io.coti.basenode.data.Hash;
+import io.coti.basenode.data.interfaces.IEntity;
 
 import java.util.List;
 import java.util.Map;
 
-public class HistoryBucketEvents{
+public class HistoryBucketEvents implements IEntity {
 
     private Hash userHash;
     private Map<EventType,List<BucketEventData>> historyBucketsEvents;
+
+    @Override
+    public Hash getHash() {
+        return userHash;
+    }
+
+    @Override
+    public void setHash(Hash hash) {
+        userHash = hash;
+    }
 }
---
+++ trustscore/src/main/java/io/coti/trustscore/model/HistoryBuckets.java
@@ -0,0 +1,15 @@
+package io.coti.trustscore.model;
+
+import io.coti.basenode.model.Collection;
+import io.coti.trustscore.data.HistoryBucketEvents;
+
+
+import javax.annotation.PostConstruct;
+
+public class HistoryBuckets extends Collection<HistoryBucketEvents> {
+
+    @PostConstruct
+    public void init() {
+        super.init();
+    }
+}
