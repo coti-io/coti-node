8c51489ea6bf03b82995a0a9fa89a1f49686bda9
cotitech <eli@coti.io>
2019-01-07 03:03:00 +0200

    Arbitrator dispute vote response fix

--- financialserver/src/main/java/io/coti/financialserver/http/data/DisputeItemVoteResponseData.java
+++ financialserver/src/main/java/io/coti/financialserver/http/data/DisputeItemVoteResponseData.java
@@ -3,9 +3,11 @@ package io.coti.financialserver.http.data;
 import io.coti.basenode.data.SignatureData;
 import io.coti.financialserver.data.DisputeItemVoteData;
 import io.coti.financialserver.data.DisputeItemVoteStatus;
+import lombok.Data;
 
 import java.time.Instant;
 
+@Data
 public class DisputeItemVoteResponseData {
     private String arbitratorHash;
     private String disputeHash;
@@ -14,6 +16,10 @@ public class DisputeItemVoteResponseData {
     private Instant voteTime;
     private SignatureData arbitratorSignature;
 
+    private DisputeItemVoteResponseData() {
+
+    }
+    
     public DisputeItemVoteResponseData(DisputeItemVoteData disputeItemVoteData) {
         this.arbitratorHash = disputeItemVoteData.getArbitratorHash().toString();
         this.disputeHash = disputeItemVoteData.getDisputeHash().toString();
