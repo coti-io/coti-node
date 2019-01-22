fe1173a671545de964e15a502f5e1285bf8066ec
cotitech <eli@coti.io>
2019-01-22 19:13:20 +0200

    itemIds added to comment and document events

--- financialserver/src/main/java/io/coti/financialserver/http/data/DocumentNameResponseData.java
+++ financialserver/src/main/java/io/coti/financialserver/http/data/DocumentNameResponseData.java
@@ -6,16 +6,21 @@ import io.coti.financialserver.http.data.interfaces.IDisputeEventResponseData;
 import lombok.Data;
 
 import java.time.Instant;
+import java.util.List;
 
 @Data
 public class DocumentNameResponseData implements IDisputeEventResponseData {
     private String hash;
+    private String disputeHash;
+    private List<Long> itemIds;
     private String fileName;
     private ActionSide uploadSide;
     private Instant creationTime;
 
     public DocumentNameResponseData(DisputeDocumentData disputeDocumentData) {
         this.hash = disputeDocumentData.getHash().toString();
+        this.disputeHash = disputeDocumentData.getDisputeHash().toString();
+        this.itemIds = disputeDocumentData.getItemIds();
         this.fileName = disputeDocumentData.getFileName();
         this.uploadSide = disputeDocumentData.getUploadSide();
         this.creationTime = disputeDocumentData.getCreationTime();
