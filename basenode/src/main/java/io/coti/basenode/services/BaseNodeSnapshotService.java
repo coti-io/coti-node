4a36e896e4491cded755ae76b286d384036e7b74
Kirill <kirill@coti.io>
2019-02-03 19:59:46 +0200

    Snapshot process - carcass 2

--- basenode/src/main/java/io/coti/basenode/services/BaseNodeSnapshotService.java
+++ basenode/src/main/java/io/coti/basenode/services/BaseNodeSnapshotService.java
@@ -1,13 +1,13 @@
-package io.coti.dspnode.services;
+package io.coti.basenode.services;
 
 import io.coti.basenode.data.PrepareForSnapshot;
-import io.coti.basenode.services.BaseNodeSnapshotService;
+import io.coti.basenode.services.interfaces.ISnapshotService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-public    class SnapshotService  extends BaseNodeSnapshotService {
+public class BaseNodeSnapshotService implements ISnapshotService {
 
     @Override
     public void handlePrepareForSnapshot(PrepareForSnapshot prepareForSnapshot) {
