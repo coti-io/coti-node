7850925c40ace85e4d3b7cc64121233f00ce0ea9
Alex Stoliar <alex@coti.io>
2019-02-04 17:52:52 +0200

    Added missing method to interface add changes to validation refactor added CLUSTERSTAMP to transaction status

--- basenode/src/main/java/io/coti/basenode/services/BaseNodeSnapshotService.java
+++ basenode/src/main/java/io/coti/basenode/services/BaseNodeSnapshotService.java
@@ -13,4 +13,9 @@ public class BaseNodeSnapshotService implements ISnapshotService {
     public void prepareForSnapshot(SnapshotPreparationData snapshotPreparationData) {
         boolean bp = true;
     }
+
+    @Override
+    public boolean getIsSnapshotInProgress() {
+        return false;
+    }
 }
