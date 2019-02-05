adc922c315c1c3893563f42c1a29b7e028e5bfb6
Kirill <kirill@coti.io>
2019-02-05 16:54:18 +0200

    ClusterStamp - full & dsp nodes ready for cluster stamp events

--- basenode/src/main/java/io/coti/basenode/data/ClusterStampPreparationData.java
+++ basenode/src/main/java/io/coti/basenode/data/ClusterStampPreparationData.java
@@ -1,17 +1,55 @@
 package io.coti.basenode.data;
 
+import io.coti.basenode.data.interfaces.IPropagatable;
+import io.coti.basenode.data.interfaces.ISignValidatable;
+import io.coti.basenode.data.interfaces.ISignable;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Data
-public class ClusterStampPreparationData extends ClusterStampStateData {
+public class ClusterStampPreparationData implements IPropagatable, ISignable, ISignValidatable {
+
+    private Hash hash;
+    private Long lastDspConfirmed;
+    private Hash zeroSpendHash;
+    private SignatureData zeroSpendSignature;
+
+    public ClusterStampPreparationData(Long lastDspConfirmed) {
+        this.hash = new Hash(lastDspConfirmed);
+        this.lastDspConfirmed = lastDspConfirmed;
+    }
 
     public ClusterStampPreparationData() {
     }
 
-    public ClusterStampPreparationData(long totalConfirmedTransactions) {
-        this.clusterStampHash = new Hash(totalConfirmedTransactions);
-        this.totalConfirmedTransactionsCount = totalConfirmedTransactions;
+    @Override
+    public Hash getHash() {
+        return hash;
+    }
+
+    @Override
+    public void setHash(Hash hash) {
+        this.hash = hash;
+    }
+
+    @Override
+    public SignatureData getSignature() {
+        return zeroSpendSignature;
+    }
+
+    @Override
+    public Hash getSignerHash() {
+        return zeroSpendHash;
+    }
+
+    @Override
+    public void setSignerHash(Hash signerHash) {
+        zeroSpendHash = signerHash;
+    }
+
+    @Override
+    public void setSignature(SignatureData signature) {
+        zeroSpendSignature = signature;
     }
 }
