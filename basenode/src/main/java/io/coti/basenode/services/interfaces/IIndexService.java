d94a0208d7d6ac73f931f9be596a47cc2b82bac5
Kirill <kirill@coti.io>
2019-02-17 12:00:41 +0200

    Cluster Stamp fixes

--- basenode/src/main/java/io/coti/basenode/services/interfaces/IIndexService.java
+++ basenode/src/main/java/io/coti/basenode/services/interfaces/IIndexService.java
@@ -3,4 +3,5 @@ package io.coti.basenode.services.interfaces;
 import io.coti.basenode.data.Hash;
 
 public interface IIndexService {
+    void incrementAndGetTotalConfirmed(Hash hash, long totalConfirmedTransactions);
 }
