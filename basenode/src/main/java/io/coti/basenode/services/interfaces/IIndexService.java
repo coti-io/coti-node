9e32ea5e51feeea1103f671285798760608b1c65
Kirill <kirill@coti.io>
2019-02-17 11:55:39 +0200

    cs

--- basenode/src/main/java/io/coti/basenode/services/interfaces/IIndexService.java
+++ basenode/src/main/java/io/coti/basenode/services/interfaces/IIndexService.java
@@ -3,6 +3,4 @@ package io.coti.basenode.services.interfaces;
 import io.coti.basenode.data.Hash;
 
 public interface IIndexService {
-
-    void incrementAndGetTotalConfirmed(Hash transactionHash, long totalConfirmedTransactions);
 }
