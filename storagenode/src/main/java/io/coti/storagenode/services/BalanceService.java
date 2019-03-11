f5fbcf390350dbe9fe1fa56f62deb320035fb480
tomerarm <46295412+tomerarm@users.noreply.github.com>
2019-03-11 18:11:14 +0200

    Updating properties and empty implementation of BalanceService
    HistoryNode and Storage node up ok

--- storagenode/src/main/java/io/coti/storagenode/services/BalanceService.java
+++ storagenode/src/main/java/io/coti/storagenode/services/BalanceService.java
@@ -9,7 +9,11 @@ public class BalanceService extends BaseNodeBalanceService {
     @Override
     public void init() throws Exception {
         // TODO: blank method, for start-up checks, to be removed later
+    }
 
+    @Override
+    public void validateBalances() {
+        // TODO: blank method, for start-up checks, to be removed later
     }
 
 }
