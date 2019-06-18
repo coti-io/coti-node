3b6ad4284a847f5088c3ad9fa8e0aadae8c131b7
cotitech <eli@coti.io>
2019-06-18 14:24:51 +0300

    history node initialization service

---
+++ historynode/src/main/java/io/coti/historynode/services/InitializationService.java
@@ -0,0 +1,68 @@
+package io.coti.historynode.services;
+
+import io.coti.basenode.crypto.NodeCryptoHelper;
+import io.coti.basenode.data.DspConsensusResult;
+import io.coti.basenode.data.NetworkNodeData;
+import io.coti.basenode.data.NodeType;
+import io.coti.basenode.data.TransactionData;
+import io.coti.basenode.data.interfaces.IPropagatable;
+import io.coti.basenode.services.BaseNodeInitializationService;
+import io.coti.basenode.services.interfaces.ICommunicationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+
+@Slf4j
+@Service
+public class InitializationService extends BaseNodeInitializationService {
+
+    @Autowired
+    private ICommunicationService communicationService;
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${server.url}")
+    private String webServerUrl;
+    private EnumMap<NodeType, List<Class<? extends IPropagatable>>> publisherNodeTypeToMessageTypesMap = new EnumMap<>(NodeType.class);
+
+    @PostConstruct
+    public void init() {
+        super.initDB();
+        super.createNetworkNodeData();
+        super.getNetwork();
+
+        publisherNodeTypeToMessageTypesMap.put(NodeType.ZeroSpendServer, Arrays.asList(TransactionData.class, DspConsensusResult.class));
+        publisherNodeTypeToMessageTypesMap.put(NodeType.FinancialServer, Arrays.asList(TransactionData.class));
+
+        communicationService.initSubscriber(NodeType.HistoryNode, publisherNodeTypeToMessageTypesMap);
+
+        NetworkNodeData zerospendNetworkNodeData = networkService.getSingleNodeData(NodeType.ZeroSpendServer);
+        if (zerospendNetworkNodeData == null) {
+            log.error("No zerospend server exists in the network got from the node manager, about to exit application");
+            System.exit(-1);
+        }
+        networkService.setRecoveryServerAddress(zerospendNetworkNodeData.getHttpFullAddress());
+        communicationService.addSubscription(zerospendNetworkNodeData.getPropagationFullAddress(), NodeType.ZeroSpendServer);
+        networkService.addListToSubscription(networkService.getMapFromFactory(NodeType.DspNode).values());
+        if (networkService.getSingleNodeData(NodeType.FinancialServer) != null) {
+            networkService.addListToSubscription(new ArrayList<>(Arrays.asList(networkService.getSingleNodeData(NodeType.FinancialServer))));
+        }
+
+        super.init();
+    }
+
+    @Override
+    protected NetworkNodeData createNodeProperties() {
+        NetworkNodeData networkNodeData = new NetworkNodeData(NodeType.HistoryNode, nodeIp, serverPort, NodeCryptoHelper.getNodeHash(), networkType);
+        networkNodeData.setWebServerUrl(webServerUrl);
+        return networkNodeData;
+    }
+
+
+}
