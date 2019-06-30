bb81acd6b2db696abaf3a3a3c28133c69d8b2280
Tomer <tomer@coti.io>
2019-06-30 17:37:10 +0300

    History 2 WIP
    transactions tests

--- historynode/src/main/java/io/coti/historynode/crypto/GetTransactionsByAddressRequestCrypto.java
+++ historynode/src/main/java/io/coti/historynode/crypto/GetTransactionsByAddressRequestCrypto.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class GetTransactionsByAddressRequestCrypto extends SignatureValidationCrypto<GetTransactionsByAddressRequest> {
 
+    //TODO 6/30/2019 tomer: remove this and leave only TransactionsRequestCrypto for the generic request
     @Override
     public byte[] getSignatureMessage(GetTransactionsByAddressRequest getTransactionsByAddressRequest) {
         byte[] addressInBytes = getTransactionsByAddressRequest.getAddress().getBytes();
--- historynode/src/main/java/io/coti/historynode/services/TransactionIndexingService.java
+++ historynode/src/main/java/io/coti/historynode/services/TransactionIndexingService.java
@@ -17,9 +17,11 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
+@Deprecated
 @Slf4j
 @Service
 public class TransactionIndexingService {
@@ -31,58 +33,68 @@ public class TransactionIndexingService {
 
 
 
-    public void addToHistoryTransactionIndexes(TransactionData transactionData) {
-        Instant attachmentTime = transactionData.getAttachmentTime();
-        Hash hashByDate = calculateHashByAttachmentTime(attachmentTime);
-        HashSet<Hash> relatedTransactionAddressHashes = getRelatedTransactionHashes(transactionData);
-
-        AddressTransactionsByDate transactionsByDateHash = addressTransactionsByDates.getByHash(hashByDate);
-        if(transactionsByDateHash== null) {
-            addressTransactionsByDates.put(new AddressTransactionsByDate(attachmentTime, relatedTransactionAddressHashes));
-        } else {
-            transactionsByDateHash.getAddresses().addAll(relatedTransactionAddressHashes);
-            addressTransactionsByDates.put(transactionsByDateHash);
-        }
-
-
-        for(Hash transactionAddressHash : relatedTransactionAddressHashes) {
-            HashMap<Hash, HashSet<Hash>> transactionHashesMap = new HashMap<>();
-            AddressTransactionsByAddress transactionsByAddressHash = addressTransactionsByAddresses.getByHash(transactionAddressHash);
-            if(transactionsByAddressHash==null) {
-                HashSet<Hash> transactionHashes = new HashSet<>();
-                if( !transactionHashes.add(transactionData.getHash()) ) {
-                    log.info("{} was already present by address", transactionData);
-                }
-                transactionHashesMap.put(hashByDate, transactionHashes);
-                addressTransactionsByAddresses.put(new AddressTransactionsByAddress(transactionAddressHash, transactionHashesMap));
-            } else {
-                HashSet<Hash> transactionsHashesByDate = transactionsByAddressHash.getTransactionHashesByDates().get(hashByDate);
-                if(transactionsHashesByDate == null) {
-                    HashSet<Hash> transactionsHashes = new HashSet<>();
-                    transactionsHashes.add(transactionData.getHash());
-                    transactionsByAddressHash.getTransactionHashesByDates().put(hashByDate, transactionsHashes);
-                } else {
-                    transactionsHashesByDate.add(transactionData.getHash());
-                }
-                addressTransactionsByAddresses.put(transactionsByAddressHash);
-            }
-        }
-    }
-
-    public HashSet<Hash> getRelatedTransactionHashes(TransactionData transactionData) {
-        HashSet<Hash> hashes = new HashSet<>();
-        hashes.add(transactionData.getSenderHash());
-        for(BaseTransactionData baseTransactionData : transactionData.getBaseTransactions()) {
-            if(baseTransactionData instanceof ReceiverBaseTransactionData) {
-                hashes.add(baseTransactionData.getAddressHash());
-            }
-        }
-        return hashes;
-    }
-
-    protected Hash calculateHashByAttachmentTime(Instant date) {
-        LocalDateTime ldt = LocalDateTime.ofInstant(date, ZoneOffset.UTC);
-        LocalDate localDate = LocalDate.of(ldt.getYear(), ldt.getMonth(),ldt.getDayOfMonth());
-        return CryptoHelper.cryptoHash(localDate.atStartOfDay().toString().getBytes());
-    }
+//    public void addToHistoryTransactionIndexes(TransactionData transactionData) {
+//        Instant attachmentTime = transactionData.getAttachmentTime();
+//        Hash hashByDate = calculateHashByAttachmentTime(attachmentTime);
+//        LocalDate attachmentLocalDate = calculateInstantLocalDate(attachmentTime);
+//        HashSet<Hash> relatedAddressHashes = getRelatedAddresses(transactionData);
+//
+//        AddressTransactionsByDate transactionsByDateHash = addressTransactionsByDates.getByHash(hashByDate);
+//        if(transactionsByDateHash== null) {
+//            HashSet<Hash> addresses = new HashSet<>();
+//            addresses.add(transactionData.getHash());
+//            addressTransactionsByDates.put(new AddressTransactionsByDate(attachmentTime, addresses));
+//        } else {
+//            transactionsByDateHash.getTransactionsAddresses().add(transactionData.getHash());
+//            addressTransactionsByDates.put(transactionsByDateHash);
+//        }
+//
+//
+//        for(Hash transactionAddressHash : relatedAddressHashes) {
+//            SortedMap<LocalDate, HashSet<Hash>> transactionHashesMap = new TreeMap<>();
+//            AddressTransactionsByAddress transactionsByAddressHash = addressTransactionsByAddresses.getByHash(transactionAddressHash);
+//            if(transactionsByAddressHash==null) {
+//                HashSet<Hash> transactionHashes = new HashSet<>();
+//                if( !transactionHashes.add(transactionData.getHash()) ) {
+//                    log.info("{} was already present by address", transactionData);
+//                }
+//                transactionHashesMap.put(attachmentLocalDate, transactionHashes);
+//                addressTransactionsByAddresses.put(new AddressTransactionsByAddress(transactionAddressHash, transactionHashesMap));
+//            } else {
+//                HashSet<Hash> transactionsHashesByDate = transactionsByAddressHash.getTransactionHashesByDates().get(hashByDate);
+//                if(transactionsHashesByDate == null) {
+//                    HashSet<Hash> transactionsHashes = new HashSet<>();
+//                    transactionsHashes.add(transactionData.getHash());
+//                    transactionsByAddressHash.getTransactionHashesByDates().put(attachmentLocalDate, transactionsHashes);
+//                } else {
+//                    transactionsHashesByDate.add(transactionData.getHash());
+//                }
+//                addressTransactionsByAddresses.put(transactionsByAddressHash);
+//            }
+//        }
+//    }
+//
+//
+//
+//    public HashSet<Hash> getRelatedAddresses(TransactionData transactionData) {
+//        HashSet<Hash> hashes = new HashSet<>();
+//        hashes.add(transactionData.getSenderHash());
+//        for(BaseTransactionData baseTransactionData : transactionData.getBaseTransactions()) {
+//            if(baseTransactionData instanceof ReceiverBaseTransactionData) {
+//                hashes.add(baseTransactionData.getAddressHash());
+//            }
+//        }
+//        return hashes;
+//    }
+//
+//    protected Hash calculateHashByAttachmentTime(Instant date) {
+//        LocalDateTime ldt = LocalDateTime.ofInstant(date, ZoneOffset.UTC);
+//        LocalDate localDate = LocalDate.of(ldt.getYear(), ldt.getMonth(),ldt.getDayOfMonth());
+//        return CryptoHelper.cryptoHash(localDate.atStartOfDay().toString().getBytes());
+//    }
+//
+//    public LocalDate calculateInstantLocalDate(Instant date) {
+//        LocalDateTime ldt = LocalDateTime.ofInstant(date, ZoneOffset.UTC);
+//        return LocalDate.of(ldt.getYear(), ldt.getMonth(),ldt.getDayOfMonth());
+//    }
 }
