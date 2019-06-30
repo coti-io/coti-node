688c9adf74a4163ff2442739f17f03d05425a600
Tomer <tomer@coti.io>
2019-06-30 17:37:10 +0300

    History 2 WIP
    transactions tests

--- historynode/src/main/java/io/coti/historynode/services/interfaces/IHistoryTransactionService.java
+++ historynode/src/main/java/io/coti/historynode/services/interfaces/IHistoryTransactionService.java
@@ -1,12 +1,12 @@
 package io.coti.historynode.services.interfaces;
 
 import io.coti.basenode.http.interfaces.IResponse;
-import io.coti.historynode.http.GetTransactionsRequest;
+import io.coti.historynode.http.GetTransactionsRequestOld;
 import org.springframework.http.ResponseEntity;
 
 public interface IHistoryTransactionService {
 
-    ResponseEntity<IResponse> getTransactionsDetails(GetTransactionsRequest getTransactionRequest);
+    ResponseEntity<IResponse> getTransactionsDetails(GetTransactionsRequestOld getTransactionRequest);
 
     void deleteLocalUnconfirmedTransactions();
 }
