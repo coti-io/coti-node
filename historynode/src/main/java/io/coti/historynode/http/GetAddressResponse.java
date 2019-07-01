f6ef777263d4d1e0bf257ebed648ede5854a71b5
Alex Stoliar <alex@coti.io>
2019-07-01 12:50:33 +0300

    added tests for getAddress in historynode address controller

--- historynode/src/main/java/io/coti/historynode/http/GetAddressResponse.java
+++ historynode/src/main/java/io/coti/historynode/http/GetAddressResponse.java
@@ -1,14 +1,21 @@
 package io.coti.historynode.http;
 
 import io.coti.basenode.data.AddressData;
+import io.coti.basenode.data.Hash;
 import io.coti.basenode.http.interfaces.IResponse;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class GetAddressResponse implements IResponse {
-    AddressData address;
+    @NotNull(message = "Address hash must not be null")
+    private Hash addressesHash;
 
-    public GetAddressResponse(AddressData address) {
+    private AddressData address;
+
+    public GetAddressResponse(Hash addressesHash,AddressData address) {
+        this.addressesHash = addressesHash;
         this.address = address;
     }
 
