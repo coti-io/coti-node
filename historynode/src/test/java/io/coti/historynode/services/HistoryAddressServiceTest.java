0273c363da1f6e135808a8473cd0e3f16d02f916
Alex Stoliar <alex@coti.io>
2019-07-01 12:50:33 +0300

    added tests for getAddress in historynode address controller

--- historynode/src/test/java/io/coti/historynode/services/HistoryAddressServiceTest.java
+++ historynode/src/test/java/io/coti/historynode/services/HistoryAddressServiceTest.java
@@ -5,6 +5,7 @@ import io.coti.basenode.data.Hash;
 import io.coti.basenode.http.GetEntitiesBulkRequest;
 import io.coti.basenode.http.GetEntityRequest;
 import io.coti.basenode.http.interfaces.IResponse;
+import io.coti.basenode.model.Addresses;
 import io.coti.historynode.http.GetAddressBatchResponse;
 import io.coti.historynode.http.GetAddressResponse;
 import io.coti.historynode.http.storageConnector.interaces.IStorageConnector;
@@ -39,32 +40,83 @@ public class HistoryAddressServiceTest {
     public static final int NUMBER_OF_ADDRESSES = 8;
 
     @Autowired
-    private IHistoryAddressService addressService;
+    private HistoryAddressService addressService;
 
     @MockBean
     private IStorageConnector storageConnector;
+
+    @MockBean
+    private Addresses addresses;
 
     @Before
     public void setUp() throws Exception {
     }
 
+    /** getAddress
+     *  Scenarios:
+     *      addressData in RocksDB - returns response entity with addressData and hash, status ok
+     *      addressData not in RocksDB and in StorageNode - returns response entity with addressData and hash, status ok
+     *      addressData not in RocksDB and not in StorageNode - returns response entity with null as addressData and hash, status ok
+    **/
     @Test
-    public void getAddress_noExceptionIsThrown() {
-        try {
-            Hash address;
-            AddressData addressData;
-            Hash hash = TestUtils.generateRandomHash();
-            address = hash;
-            addressData = new AddressData(hash);
+    public void getAddress(){
+        getAddress_AddressInRocksDb_returnFoundAddress();
+        getAddress_AddressNotInRocksDb_returnFoundAddress();
+        getAddress_AddressNotInRocksDbOrStorage_returnNotFoundAddress();
 
-            ResponseEntity<IResponse> response = ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(new GetAddressResponse(addressData));
-            when(storageConnector.getForObject(any(String.class), any(Class.class), any(GetEntityRequest.class)))
-                    .thenReturn(response);
-            addressService.getAddress(address);
-        } catch (Exception e) {
-            Assert.fail(e.getMessage());
-        }
     }
+
+    public void getAddress_AddressInRocksDb_returnFoundAddress() {
+        Hash hash = TestUtils.generateRandomHash();
+        AddressData addressData = new AddressData(hash);
+        when(addresses.getByHash(hash)).thenReturn(addressData);
+        ResponseEntity<IResponse> response = addressService.getAddress(hash);
+        Assert.assertEquals(response,ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new GetAddressResponse(hash,addressData)));
+    }
+
+    public void getAddress_AddressNotInRocksDb_returnFoundAddress() {
+        Hash hash = TestUtils.generateRandomHash();
+        AddressData addressData = new AddressData(hash);
+        when(addresses.getByHash(hash)).thenReturn(null);
+        Assert.assertEquals(null, addressService.getAddress(hash));
+        ResponseEntity<IResponse> response = ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new GetAddressResponse(hash,addressData));
+//        when(addressService.getAddressFromStorage(hash)).thenReturn(response);
+        Assert.assertEquals(response, addressService.getAddress(hash));
+    }
+
+    public void getAddress_AddressNotInRocksDbOrStorage_returnNotFoundAddress() {
+        Hash hash = TestUtils.generateRandomHash();
+        AddressData addressData = new AddressData(hash);
+        when(addresses.getByHash(hash)).thenReturn(null);
+        Assert.assertEquals(null, addressService.getAddress(hash));
+        ResponseEntity<IResponse> response = ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new GetAddressResponse(hash,null));
+        //when(addressService.getAddressFromStorage(hash)).thenReturn(response);
+        Assert.assertEquals(response, addressService.getAddress(hash));
+    }
+
+//    @Test
+//    public void getAddress_noExceptionIsThrown() {
+//        try {
+//            Hash address;
+//            AddressData addressData;
+//            Hash hash = TestUtils.generateRandomHash();
+//            address = hash;
+//            addressData = new AddressData(hash);
+//
+//            ResponseEntity<IResponse> response = ResponseEntity
+//                    .status(HttpStatus.OK)
+//                    .body(new GetAddressResponse(hash, addressData));
+//            when(storageConnector.getForObject(any(String.class), any(Class.class), any(GetEntityRequest.class)))
+//                    .thenReturn(response);
+//            addressService.getAddressFromStorage(address);
+//        } catch (Exception e) {
+//            Assert.fail(e.getMessage());
+//        }
+//    }
 }
