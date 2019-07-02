543088d5a96952fb46cec8f898c00922d4f9fa9d
Tomer <tomer@coti.io>
2019-07-02 14:27:15 +0300

    Auto stash before rebase of "origin/history2"
    NPE from Elastic search partial fix
    Updating pom.xml
    
    Updating pom.xml
    History 2 cleanups
    
    cleanups
    History 2 
    
    Get Transactions by Date or Address, different endpoints
    Renaming requests + post-mapping

--- historynode/src/main/java/io/coti/historynode/data/AddressTransactionsByAddress.java
+++ historynode/src/main/java/io/coti/historynode/data/AddressTransactionsByAddress.java
@@ -4,21 +4,19 @@ import io.coti.basenode.data.Hash;
 import io.coti.basenode.data.interfaces.IEntity;
 import lombok.Data;
 
-import java.time.Instant;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.SortedMap;
 
 @Data
 public class AddressTransactionsByAddress implements IEntity {
 
     private Hash address;
-//    private HashMap<Hash, HashSet<Hash>> transactionHashesByDates;
-    private SortedMap<LocalDate, HashSet<Hash>> transactionHashesByDates;
+    private HashMap<LocalDate, HashSet<Hash>> transactionHashesByDates;
+//    private SortedMap<LocalDate, HashSet<Hash>> transactionHashesByDates;
 
 
-    public AddressTransactionsByAddress(Hash address, SortedMap<LocalDate, HashSet<Hash>> transactionHashesByDate) {
+    public AddressTransactionsByAddress(Hash address, HashMap<LocalDate, HashSet<Hash>> transactionHashesByDate) {
         this.address = address;
         this.transactionHashesByDates = transactionHashesByDate;
     }
