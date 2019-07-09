4d2ecafd2761d171b4635ee2b422a103ed29f6a1
Alex Stoliar <alex@coti.io>
2019-07-09 16:19:43 +0300

    WIP - before integration of history and storage address flow

--- basenode/src/main/java/io/coti/basenode/http/GetAddressesResponse.java
+++ basenode/src/main/java/io/coti/basenode/http/GetAddressesResponse.java
@@ -6,23 +6,26 @@ import io.coti.basenode.data.SignatureData;
 import io.coti.basenode.data.interfaces.ISignValidatable;
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
 public class GetAddressesResponse extends Response implements ISignValidatable {
-    @NotEmpty
+    //@NotEmpty
     private Map<Hash,AddressData> addressHashesToAddresses;
-    @NotNull
+    //@NotNull
     private Hash userHash;
-    @NotNull
+    //@NotNull
     private SignatureData userSignature;
 
     public GetAddressesResponse(Map<Hash,AddressData> addressHashesToAddresses,String message, String status) {
         super(message, status);
         this.addressHashesToAddresses = addressHashesToAddresses;
 
+    }
+
+    public GetAddressesResponse(){
+        addressHashesToAddresses = new HashMap<>();
     }
 
     @Override
