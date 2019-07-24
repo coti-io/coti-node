4a621a39df6a2432af922b0228722b48031ce30f
Tomer <tomer@coti.io>
2019-07-24 17:50:28 +0300

    History 2 start date per address
    Request transactions by address with partial dates
    Tests adjustments WIP
    Transactions Indexing tests
    Transactions tests cleanups
    History 2 Tests WIP
    
    Tests WIP

--- historynode/src/main/java/io/coti/historynode/data/AddressTransactionsByAddress.java
+++ historynode/src/main/java/io/coti/historynode/data/AddressTransactionsByAddress.java
@@ -4,6 +4,7 @@ import io.coti.basenode.data.Hash;
 import io.coti.basenode.data.interfaces.IEntity;
 import lombok.Data;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,10 +14,12 @@ public class AddressTransactionsByAddress implements IEntity {
 
     private Hash address;
     private HashMap<LocalDate, HashSet<Hash>> transactionHashesByDates;
+    private Instant startDate;
 
-    public AddressTransactionsByAddress(Hash address, HashMap<LocalDate, HashSet<Hash>> transactionHashesByDate) {
+    public AddressTransactionsByAddress(Hash address, HashMap<LocalDate, HashSet<Hash>> transactionHashesByDate, Instant startDate) {
         this.address = address;
         this.transactionHashesByDates = transactionHashesByDate;
+        this.startDate = startDate;
     }
 
     @Override
--- storagenode/src/test/java/io/coti/storagenode/services/TransactionServiceTest.java
+++ storagenode/src/test/java/io/coti/storagenode/services/TransactionServiceTest.java
@@ -77,6 +77,9 @@ public class TransactionServiceTest {
     @MockBean
     private AddressStorageService addressStorageService;
 
+    @MockBean
+    private ChunkingService chunkingService;
+
 
     private ObjectMapper mapper;
 
