2a993469d20f8ea0344096dd2026507dd4955aed
cotitech <eli@coti.io>
2019-07-29 11:09:01 +0300

    fix spelling mistake

--- basenode/src/main/java/io/coti/basenode/http/SerializableRequest.java
+++ basenode/src/main/java/io/coti/basenode/http/SerializableRequest.java
@@ -2,5 +2,5 @@ package io.coti.basenode.http;
 
 import io.coti.basenode.http.interfaces.ISerializable;
 
-public class SeriazableRequest extends Request implements ISerializable {
+public class SerializableRequest extends Request implements ISerializable {
 }
