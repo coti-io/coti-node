18ee93283f3f23d2d7327d3e6593f6b2ec4863e6
Alex Stoliar <alex@coti.io>
2019-07-29 15:31:14 +0300

    squash. full node addresses flow with basic tests
    check if addresses exists full node flow untested
    
    optimization and todo comment
    
    small fixes. added basic working tests
    
    test properties

---
+++ fullnode/src/main/java/io/coti/fullnode/data/RequestedAddressHashData.java
@@ -0,0 +1,33 @@
+package io.coti.fullnode.data;
+
+import io.coti.basenode.data.Hash;
+import io.coti.basenode.data.interfaces.IEntity;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+
+@Data
+public class RequestedAddressHashData implements IEntity {
+
+    private static final long serialVersionUID = 8458125010309122807L;
+    @NotNull
+    private Hash addressHash;
+    @NotNull
+    private Instant lastUpdateTime;
+
+    public RequestedAddressHashData(Hash addressHash) {
+        this.addressHash = addressHash;
+        this.lastUpdateTime = Instant.now();
+    }
+
+    @Override
+    public Hash getHash() {
+        return addressHash;
+    }
+
+    @Override
+    public void setHash(Hash hash) {
+        this.addressHash = hash;
+    }
+}
