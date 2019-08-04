6ab76f77b0b86c422371c7e4cd5101e5e6430c0f
cotitech <eli@coti.io>
2019-08-04 14:57:04 +0300

    code review

--- historynode/src/main/java/io/coti/historynode/data/AddressTransactionsByAddress.java
+++ historynode/src/main/java/io/coti/historynode/data/AddressTransactionsByAddress.java
@@ -4,7 +4,6 @@ import io.coti.basenode.data.Hash;
 import io.coti.basenode.data.interfaces.IEntity;
 import lombok.Data;
 
-import java.time.Instant;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -14,9 +13,9 @@ public class AddressTransactionsByAddress implements IEntity {
 
     private Hash address;
     private HashMap<LocalDate, HashSet<Hash>> transactionHashesByDates;
-    private Instant startDate;
+    private LocalDate startDate;
 
-    public AddressTransactionsByAddress(Hash address, HashMap<LocalDate, HashSet<Hash>> transactionHashesByDate, Instant startDate) {
+    public AddressTransactionsByAddress(Hash address, HashMap<LocalDate, HashSet<Hash>> transactionHashesByDate, LocalDate startDate) {
         this.address = address;
         this.transactionHashesByDates = transactionHashesByDate;
         this.startDate = startDate;
