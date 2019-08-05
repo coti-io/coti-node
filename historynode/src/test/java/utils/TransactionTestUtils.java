a23ab1f65b8fa04530ef1d8690ebfe08851846ec
Tomer <tomer@coti.io>
2019-08-05 15:33:23 +0300

    History 2 tests
    Integration tests separation, removed cluster stamp related
    Tests fixes

--- historynode/src/test/java/utils/TransactionTestUtils.java
+++ historynode/src/test/java/utils/TransactionTestUtils.java
@@ -1,50 +1,96 @@
 package utils;
 
+import io.coti.basenode.crypto.CryptoHelper;
 import io.coti.basenode.data.*;
+import io.coti.basenode.http.GetHistoryAddressesRequest;
 
 import java.math.BigDecimal;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static utils.TestConstants.*;
 
 public class TransactionTestUtils {
 
-    public static TransactionData createRandomTransaction(Hash hash) {
-        ArrayList<BaseTransactionData> baseTransactions = new ArrayList<>(
-                Collections.singletonList(new InputBaseTransactionData(
-                        HashTestUtils.generateRandomHash(TestConstants.SIZE_OF_BASE_TRANSACTION_HASH),
-                        new BigDecimal(0),
-                        Instant.now())));
-        TransactionData transactionData = new TransactionData(baseTransactions,
-                hash,
-                TestConstants.TRANSACTION_DESCRIPTION,
-                generateRandomTrustScore(),
-                Instant.now(),
-                TransactionType.Payment);
-        return transactionData;
+
+    public static Hash generateRandomHash() {
+        return generateRandomHash(SIZE_OF_HASH);
+    }
+
+    public static Hash generateRandomAddressHash() {
+        return generateRandomHash(SIZE_OF_ADDRESS_HASH);
+    }
+
+    public static Hash generateRandomHash(int lengthOfHash) {
+        StringBuilder hexa = new StringBuilder();
+        for (int i = 0; i < lengthOfHash; i++) {
+            int randomNum = ThreadLocalRandom.current().nextInt(0, 15 + 1);
+            hexa.append(hexaOptions[randomNum]);
+        }
+        return new Hash(hexa.toString());
     }
 
     public static TransactionData createRandomTransaction() {
-        return createRandomTransaction(HashTestUtils.generateRandomHash(TestConstants.SIZE_OF_HASH));
+        return createRandomTransaction(generateRandomHash(SIZE_OF_HASH));
     }
 
-    public static Double generateRandomTrustScore() {
-        return Math.random() * TestConstants.MAX_TRUST_SCORE;
+    public static TransactionData createRandomTransactionWithSenderAddress() {
+        TransactionData transactionData = createRandomTransaction(generateRandomHash(SIZE_OF_HASH));
+        transactionData.setSenderHash(generateRandomHash());
+        return transactionData;
     }
 
-    public static List<TransactionData> generateListOfRandomTransactionData(int listSize) {
-        List<TransactionData> transactions = new ArrayList<>();
-        for (int i = 0; i <= listSize; i++) {
-            transactions.add(createRandomTransaction());
+    public static TransactionData createRandomTransactionWithSenderAddress(Hash hash) {
+        TransactionData transactionData = createRandomTransaction(generateRandomHash(SIZE_OF_HASH));
+        transactionData.setSenderHash(hash);
+        return transactionData;
+    }
+
+    private static TransactionData createRandomTransaction(Hash hash) {
+        ArrayList<BaseTransactionData> baseTransactions = new ArrayList<>(
+                Collections.singletonList(new InputBaseTransactionData
+                        (HashTestUtils.generateRandomAddressHash(),
+                                new BigDecimal(0),
+                                Instant.now())));
+        byte[] bytesToHash = getBaseMessageInBytes(baseTransactions.get(0));
+        baseTransactions.get(0).setHash(CryptoHelper.cryptoHash(bytesToHash));
+        return new TransactionData(baseTransactions,
+                hash,
+                TRANSACTION_DESCRIPTION,
+                generateRandomTrustScore(),
+                Instant.now(),
+                TransactionType.Transfer);
+    }
+
+    protected static byte[] getBaseMessageInBytes(BaseTransactionData baseTransactionData) {
+        byte[] addressBytes = baseTransactionData.getAddressHash().getBytes();
+        String decimalStringRepresentation = baseTransactionData.getAmount().stripTrailingZeros().toPlainString();
+        byte[] bytesOfAmount = decimalStringRepresentation.getBytes(StandardCharsets.UTF_8);
+
+        Instant createTime = baseTransactionData.getCreateTime();
+        byte[] createTimeInBytes = ByteBuffer.allocate(Long.BYTES).putLong(createTime.toEpochMilli()).array();
+
+        ByteBuffer baseTransactionBuffer = ByteBuffer.allocate(addressBytes.length + bytesOfAmount.length + createTimeInBytes.length).
+                put(addressBytes).put(bytesOfAmount).put(createTimeInBytes);
+
+        return baseTransactionBuffer.array();
+    }
+
+    private static Double generateRandomTrustScore() {
+        return Math.random() * 100;
+    }
+
+    public static GetHistoryAddressesRequest generateGetAddressesRequest() {
+        List<Hash> hashes = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            hashes.add(generateRandomHash());
         }
-        return transactions;
-    }
-
-    public static Set<TransactionData> generateSetOfRandomTransactionData(int listSize) {
-        Set<TransactionData> transactions = new HashSet<>();
-        for (int i = 0; i < listSize; i++) {
-            transactions.add(createRandomTransaction());
-        }
-        return transactions;
+        return new GetHistoryAddressesRequest(hashes);
     }
 
 }
