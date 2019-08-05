457631fcca7a305be0ab87b25e035ea44052b141
Tomer <tomer@coti.io>
2019-08-05 18:50:00 +0300

    tests

--- historynode/src/test/java/io/coti/historynode/services/HistoryTransactionServiceTest.java
+++ historynode/src/test/java/io/coti/historynode/services/HistoryTransactionServiceTest.java
@@ -7,7 +7,6 @@ import io.coti.basenode.crypto.TransactionSenderCrypto;
 import io.coti.basenode.crypto.TransactionTrustScoreCrypto;
 import io.coti.basenode.data.Hash;
 import io.coti.basenode.data.TransactionData;
-import io.coti.basenode.data.interfaces.IPropagatable;
 import io.coti.basenode.database.BaseNodeRocksDBConnector;
 import io.coti.basenode.database.interfaces.IDatabaseConnector;
 import io.coti.basenode.http.HttpJacksonSerializer;
@@ -20,7 +19,9 @@ import io.coti.basenode.services.interfaces.ITransactionHelper;
 import io.coti.basenode.services.liveview.LiveViewService;
 import io.coti.historynode.crypto.GetTransactionsByAddressRequestCrypto;
 import io.coti.historynode.data.AddressTransactionsByAddress;
+import io.coti.historynode.data.AddressTransactionsByDate;
 import io.coti.historynode.http.GetTransactionsByAddressRequest;
+import io.coti.historynode.http.GetTransactionsByDateRequest;
 import io.coti.historynode.model.AddressTransactionsByAddresses;
 import io.coti.historynode.model.AddressTransactionsByDates;
 import org.junit.Assert;
@@ -72,6 +73,7 @@ import static utils.TransactionTestUtils.generateRandomHash;
 public class HistoryTransactionServiceTest {
 
     public static final String EMPTY_OUTPUT = "[]";
+
     @Value("${storage.server.address}")
     protected String storageServerAddress;
     @Value("${global.private.key}")
@@ -239,7 +241,7 @@ public class HistoryTransactionServiceTest {
         when(getTransactionsByAddressRequestCrypto.verifySignature(any(GetTransactionsByAddressRequest.class))).thenReturn(Boolean.FALSE);
 
         transactionService.getTransactionsByAddress(request, response);
-        Assert.assertEquals(response.getStatus(), HttpStatus.UNAUTHORIZED.value());
+        Assert.assertEquals(HttpStatus.UNAUTHORIZED.value(), response.getStatus());
     }
 
     @Test
@@ -248,12 +250,12 @@ public class HistoryTransactionServiceTest {
         MockHttpServletResponse response = new MockHttpServletResponse();
         when(getTransactionsByAddressRequestCrypto.verifySignature(any(GetTransactionsByAddressRequest.class))).thenReturn(Boolean.TRUE);
         transactionService.getTransactionsByAddress(request, response);
-        Assert.assertEquals(response.getStatus(), HttpStatus.OK.value());
-        Assert.assertEquals(response.getContentAsString(), EMPTY_OUTPUT);
+        Assert.assertEquals(HttpStatus.OK.value(), response.getStatus());
+        Assert.assertEquals(EMPTY_OUTPUT, response.getContentAsString());
     }
 
     @Test
-    public void getTransactionsByAddress_validSignatureWithAddress_authorizedEmptyResponse() throws UnsupportedEncodingException {
+    public void getTransactionsByAddress_validSignatureWithAddress_authorizedNotEmptyResponseFromLocal() throws UnsupportedEncodingException {
         GetTransactionsByAddressRequest request = new GetTransactionsByAddressRequest();
         MockHttpServletResponse response = new MockHttpServletResponse();
         when(getTransactionsByAddressRequestCrypto.verifySignature(any(GetTransactionsByAddressRequest.class))).thenReturn(Boolean.TRUE);
@@ -268,11 +270,75 @@ public class HistoryTransactionServiceTest {
         TransactionData transactionData = TransactionTestUtils.createRandomTransaction();
         when(transactions.getByHash((transactionHashes.stream().collect(Collectors.toList())).get(0))).thenReturn(transactionData);
         transactionService.getTransactionsByAddress(request, response);
-        Assert.assertEquals(response.getStatus(), HttpStatus.OK.value());
+        Assert.assertEquals(HttpStatus.OK.value(), response.getStatus());
 
-//        IPropagatable deserializeTransaction = jacksonSerializer.deserialize(response.getContentAsString());
-        Assert.assertNotEquals(response.getContentAsString(), EMPTY_OUTPUT);
+        Assert.assertNotEquals(EMPTY_OUTPUT, response.getContentAsString());
     }
 
+    @Test
+    public void getTransactionsByDate_noDate_emptyResponse() throws UnsupportedEncodingException {
+        GetTransactionsByDateRequest request = new GetTransactionsByDateRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        transactionService.getTransactionsByDate(request, response);
+
+        Assert.assertEquals(HttpStatus.OK.value(), response.getStatus());
+        Assert.assertEquals(EMPTY_OUTPUT, response.getContentAsString());
+    }
+
+    @Test
+    public void getTransactionsByDate_noTransactionsInDate_emptyResponse() throws UnsupportedEncodingException {
+        GetTransactionsByDateRequest request = new GetTransactionsByDateRequest();
+        Instant today = Instant.now();
+        request.setDate(transactionService.calculateInstantLocalDate(today));
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        when(addressTransactionsByDates.getByHash(transactionService.calculateHashByTime(today))).thenReturn(null);
+
+        transactionService.getTransactionsByDate(request, response);
+        Assert.assertEquals(HttpStatus.OK.value(), response.getStatus());
+        Assert.assertEquals(EMPTY_OUTPUT, response.getContentAsString());
+
+        response = new MockHttpServletResponse();
+        AddressTransactionsByDate addressTransactionsByDate = new AddressTransactionsByDate(today, null);
+        when(addressTransactionsByDates.getByHash(transactionService.calculateHashByTime(today))).thenReturn(addressTransactionsByDate);
+
+        transactionService.getTransactionsByDate(request, response);
+        Assert.assertEquals(HttpStatus.OK.value(), response.getStatus());
+        Assert.assertEquals(EMPTY_OUTPUT, response.getContentAsString());
+
+        response = new MockHttpServletResponse();
+        HashSet<Hash> transactionHashes = new HashSet<>();
+        addressTransactionsByDate = new AddressTransactionsByDate(today, transactionHashes);
+        when(addressTransactionsByDates.getByHash(transactionService.calculateHashByTime(today))).thenReturn(addressTransactionsByDate);
+
+        transactionService.getTransactionsByDate(request, response);
+        Assert.assertEquals(HttpStatus.OK.value(), response.getStatus());
+        Assert.assertEquals(EMPTY_OUTPUT, response.getContentAsString());
+    }
+
+
+    @Test
+    public void getTransactionsByDate_transactionsInDate_notEmptyResponse() throws UnsupportedEncodingException {
+        GetTransactionsByDateRequest request = new GetTransactionsByDateRequest();
+        Instant today = Instant.now();
+        request.setDate(transactionService.calculateInstantLocalDate(today));
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        when(addressTransactionsByDates.getByHash(transactionService.calculateHashByTime(today))).thenReturn(null);
+
+        HashSet<Hash> transactionHashes = new HashSet<>();
+        Hash transactionHashToRetrieveLocally = generateRandomHash();
+        transactionHashes.add(transactionHashToRetrieveLocally);
+
+        AddressTransactionsByDate addressTransactionsByDate = new AddressTransactionsByDate(today, transactionHashes);
+        when(addressTransactionsByDates.getByHash(transactionService.calculateHashByTime(today))).thenReturn(addressTransactionsByDate);
+        TransactionData transactionData = TransactionTestUtils.createRandomTransaction();
+        when(transactions.getByHash(any(Hash.class))).thenReturn(transactionData);
+
+        transactionService.getTransactionsByDate(request, response);
+        Assert.assertEquals(HttpStatus.OK.value(), response.getStatus());
+        Assert.assertNotEquals(EMPTY_OUTPUT, response.getContentAsString());
+    }
 
 }
