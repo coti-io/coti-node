7d0bc4e99b708915a868add34bb858c30475c4b5
Alex Stoliar <alex@coti.io>
2019-08-12 12:34:10 +0300

    WIP adding names and currencies collections

--- basenode/src/main/java/io/coti/basenode/database/BaseNodeRocksDBConnector.java
+++ basenode/src/main/java/io/coti/basenode/database/BaseNodeRocksDBConnector.java
@@ -3,8 +3,8 @@ package io.coti.basenode.database;
 import io.coti.basenode.data.Hash;
 import io.coti.basenode.data.interfaces.IEntity;
 import io.coti.basenode.database.interfaces.IDatabaseConnector;
-import io.coti.basenode.model.*;
 import io.coti.basenode.model.Collection;
+import io.coti.basenode.model.*;
 import lombok.extern.slf4j.Slf4j;
 import org.rocksdb.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,7 +48,9 @@ public class BaseNodeRocksDBConnector implements IDatabaseConnector {
                 AddressTransactionsHistories.class.getName(),
                 TransactionIndexes.class.getName(),
                 TransactionVotes.class.getName(),
-                NodeRegistrations.class.getName()
+                NodeRegistrations.class.getName(),
+                Currencies.class.getName(),
+                ClusterStampNames.class.getName()
         ));
 
     }
---
+++ basenode/src/main/java/io/coti/basenode/model/ClusterStampNames.java
@@ -0,0 +1,13 @@
+package io.coti.basenode.model;
+
+import io.coti.basenode.data.ClusterStampNameData;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClusterStampNames extends Collection<ClusterStampNameData> {
+
+    public void init() {
+        super.init();
+    }
+
+}
