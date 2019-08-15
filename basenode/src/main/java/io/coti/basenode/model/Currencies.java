360c80ae6f8f5aeeb4b922475b4221e7974ff96f
cotitech <eli@coti.io>
2019-08-15 15:46:58 +0300

    WIP adding names and currencies collections

---
+++ basenode/src/main/java/io/coti/basenode/model/Currencies.java
@@ -0,0 +1,13 @@
+package io.coti.basenode.model;
+
+import io.coti.basenode.data.CurrencyData;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Currencies extends Collection<CurrencyData> {
+
+    public void init() {
+        super.init();
+    }
+
+}
