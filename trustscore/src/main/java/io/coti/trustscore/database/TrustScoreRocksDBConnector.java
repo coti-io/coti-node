04bf1d5aa5d465cc03603b70a427a944a1f86612
Anton <anton@coti.io>
2019-09-03 12:05:06 +0300

    just downloader to file

--- trustscore/src/main/java/io/coti/trustscore/database/TrustScoreRocksDBConnector.java
+++ trustscore/src/main/java/io/coti/trustscore/database/TrustScoreRocksDBConnector.java
@@ -1,12 +1,18 @@
 package io.coti.trustscore.database;
 
 import io.coti.basenode.database.BaseNodeRocksDBConnector;
+import io.coti.trustscore.data.TrustScoreData;
 import io.coti.trustscore.model.BucketEvents;
 import io.coti.trustscore.model.MerchantRollingReserveAddresses;
 import io.coti.trustscore.model.TrustScores;
+import org.rocksdb.RocksIterator;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.util.SerializationUtils;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
 @Primary
@@ -22,4 +28,38 @@ public class TrustScoreRocksDBConnector extends BaseNodeRocksDBConnector {
                 MerchantRollingReserveAddresses.class.getName()
         ));
     }
+    @Override
+    public void init(String dbPath) {
+        super.init(dbPath);
+
+        userListDownloader();
+    }
+
+    private void userListDownloader() {
+        RocksIterator iterator;
+
+        iterator = getIterator(TrustScores.class.getName());
+        iterator.seekToFirst();
+
+        PrintWriter writer = null;
+        if (iterator.isValid()){
+            try {
+                writer = new PrintWriter("userhashlist.txt", "UTF-8");
+            } catch (FileNotFoundException | UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        while (iterator.isValid()) {
+            TrustScoreData trustScoreData = (TrustScoreData) SerializationUtils.deserialize(iterator.value());
+            writer.println(trustScoreData.getHash().toHexString());
+            iterator.next();
+        }
+        iterator.close();
+
+        if(writer!=null) {
+            writer.close();
+        }
+    }
 }
+
