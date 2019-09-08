e086ddc73712ffb452ad2d01ee54d4b66e667b9e
cotitech <eli@coti.io>
2019-09-08 14:05:28 +0300

    fix validate version validity of recovery clusterstamp

--- basenode/src/main/java/io/coti/basenode/services/BaseNodeClusterStampService.java
+++ basenode/src/main/java/io/coti/basenode/services/BaseNodeClusterStampService.java
@@ -280,7 +280,7 @@ public class BaseNodeClusterStampService implements IClusterStampService {
     }
 
     private void handleRequiredClusterStampFiles(GetClusterStampFileNamesResponse getClusterStampFileNamesResponse, boolean isStartup) {
-        if(!validateResponseVersionValidity(getClusterStampFileNamesResponse)){
+        if (!validateResponseVersionValidity(getClusterStampFileNamesResponse)) {
             throw new ClusterStampException("Recovery clusterstamp version is not valid");
         }
         if (majorClusterStampName == null) {
@@ -292,13 +292,9 @@ public class BaseNodeClusterStampService implements IClusterStampService {
 
     private boolean validateResponseVersionValidity(GetClusterStampFileNamesResponse getClusterStampFileNamesResponse) {
         LastClusterStampVersionData lastVersionData = lastClusterStampVersions.get();
-        if(!validateVersion(lastVersionData.getVersionTimeMillis(), getClusterStampFileNamesResponse.getMajor().getVersionTimeMillis())){
-            return false;
-        }
-        if(getClusterStampFileNamesResponse.getTokenClusterStampNames().stream().anyMatch(clusterStampNameData ->  !validateVersion(lastVersionData.getVersionTimeMillis(), clusterStampNameData.getVersionTimeMillis()))){
-            return false;
-        }
-        return true;
+        return lastVersionData != null
+                && validateVersion(lastVersionData.getVersionTimeMillis(), getClusterStampFileNamesResponse.getMajor().getVersionTimeMillis())
+                && getClusterStampFileNamesResponse.getTokenClusterStampNames().stream().allMatch(clusterStampNameData -> validateVersion(lastVersionData.getVersionTimeMillis(), clusterStampNameData.getVersionTimeMillis()));
     }
 
 
@@ -307,7 +303,7 @@ public class BaseNodeClusterStampService implements IClusterStampService {
     }
 
 
-    private void  handleMissingClusterStampsWithMajorNotPresent(GetClusterStampFileNamesResponse getClusterStampFileNamesResponse, boolean isStartup){
+    private void handleMissingClusterStampsWithMajorNotPresent(GetClusterStampFileNamesResponse getClusterStampFileNamesResponse, boolean isStartup) {
         clearClusterStampNamesAndFiles();
         downloadAndAddSingleClusterStamp(getClusterStampFileNamesResponse.getMajor());
         downloadAndAddClusterStamps(getClusterStampFileNamesResponse.getTokenClusterStampNames());
