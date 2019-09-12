50afc39975e3d8417724939f5f4a11bdb546aabe
cotitech <eli@coti.io>
2019-09-12 19:09:38 +0300

    BalanceService changes for multi currency

--- basenode/src/main/java/io/coti/basenode/services/BaseNodeBalanceService.java
+++ basenode/src/main/java/io/coti/basenode/services/BaseNodeBalanceService.java
@@ -15,14 +15,17 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+
+import static io.coti.basenode.services.CurrencyService.NATIVE_CURRENCY_HASH;
 
 @Slf4j
 @Service
 public class BaseNodeBalanceService implements IBalanceService {
 
-    protected Map<Hash, BigDecimal> balanceMap;
-    protected Map<Hash, BigDecimal> preBalanceMap;
+    protected Map<Hash, Map<Hash, BigDecimal>> balanceMap;
+    protected Map<Hash, Map<Hash, BigDecimal>> preBalanceMap;
 
     public void init() {
         balanceMap = new ConcurrentHashMap<>();
@@ -32,28 +35,34 @@ public class BaseNodeBalanceService implements IBalanceService {
 
     @Override
     public synchronized boolean checkBalancesAndAddToPreBalance(List<BaseTransactionData> baseTransactions) {
-        Map<Hash, BigDecimal> balanceInChangeMap = new HashMap<>();
-        Map<Hash, BigDecimal> preBalanceInChangeMap = new HashMap<>();
+        Map<Hash, Map<Hash, BigDecimal>> balanceInChangeMap = new HashMap<>();
+        Map<Hash, Map<Hash, BigDecimal>> preBalanceInChangeMap = new HashMap<>();
         for (BaseTransactionData baseTransactionData : baseTransactions) {
 
             BigDecimal amount = baseTransactionData.getAmount();
             Hash addressHash = baseTransactionData.getAddressHash();
-            balanceInChangeMap.putIfAbsent(addressHash, balanceMap.containsKey(addressHash) ? balanceMap.get(addressHash) : BigDecimal.ZERO);
-            preBalanceInChangeMap.putIfAbsent(addressHash, preBalanceMap.containsKey(addressHash) ? preBalanceMap.get(addressHash) : BigDecimal.ZERO);
-            if (amount.add(balanceInChangeMap.get(addressHash)).signum() < 0) {
-                log.error("Error in Balance check. Address {}  amount {} current Balance {} ", addressHash,
-                        amount, balanceInChangeMap.get(addressHash));
+            Hash currencyHash = getNativeCurrencyHashIfNull(baseTransactionData.getCurrencyHash());
+            balanceInChangeMap.putIfAbsent(addressHash, new ConcurrentHashMap<>());
+            balanceInChangeMap.get(addressHash).putIfAbsent(currencyHash, getBalance(addressHash, currencyHash));
+            preBalanceInChangeMap.putIfAbsent(addressHash, new ConcurrentHashMap<>());
+            preBalanceInChangeMap.get(addressHash).putIfAbsent(currencyHash, getPreBalance(addressHash, currencyHash));
+            if (amount.add(balanceInChangeMap.get(addressHash).get(currencyHash)).signum() < 0) {
+                log.error("Error in Balance check. Address {},  amount {}, current Balance {} ", addressHash,
+                        amount, balanceInChangeMap.get(addressHash).get(currencyHash));
                 return false;
             }
-            if (amount.add(preBalanceInChangeMap.get(addressHash)).signum() < 0) {
-                log.error("Error in PreBalance check. Address {}  amount {} current PreBalance {} ", addressHash,
-                        amount, preBalanceInChangeMap.get(addressHash));
+            if (amount.add(preBalanceInChangeMap.get(addressHash).get(currencyHash)).signum() < 0) {
+                log.error("Error in PreBalance check. Address {},  amount {}, current PreBalance {} ", addressHash,
+                        amount, preBalanceInChangeMap.get(addressHash).get(currencyHash));
                 return false;
             }
-            preBalanceInChangeMap.put(addressHash, amount.add(preBalanceInChangeMap.get(addressHash)));
+            preBalanceInChangeMap.get(addressHash).put(currencyHash, amount.add(preBalanceInChangeMap.get(addressHash).get(currencyHash)));
         }
-        preBalanceInChangeMap.forEach((addressHash, preBalanceInChange) -> {
-            preBalanceMap.put(addressHash, preBalanceInChange);
+        preBalanceInChangeMap.forEach((addressHash, currencyHashPreBalanceMap) -> {
+            preBalanceMap.putIfAbsent(addressHash, new ConcurrentHashMap<>());
+            currencyHashPreBalanceMap.forEach((currencyHash, preBalanceInChange) -> {
+                preBalanceMap.get(addressHash).put(currencyHash, preBalanceInChange);
+            });
             continueHandleBalanceChanges(addressHash);
         });
         return true;
@@ -68,45 +77,58 @@ public class BaseNodeBalanceService implements IBalanceService {
         GetBalancesResponse getBalancesResponse = new GetBalancesResponse();
         BigDecimal balance;
         BigDecimal preBalance;
-        for (Hash hash : getBalancesRequest.getAddresses()) {
-            balance = balanceMap.containsKey(hash) ? balanceMap.get(hash) : new BigDecimal(0);
-            preBalance = preBalanceMap.containsKey(hash) ? preBalanceMap.get(hash) : new BigDecimal(0);
-            getBalancesResponse.addAddressBalanceToResponse(hash, balance, preBalance);
+        for (Hash addressHash : getBalancesRequest.getAddresses()) {
+            balance = getBalance(addressHash, NATIVE_CURRENCY_HASH);
+            preBalance = getPreBalance(addressHash, NATIVE_CURRENCY_HASH);
+            getBalancesResponse.addAddressBalanceToResponse(addressHash, balance, preBalance);
         }
         return ResponseEntity.status(HttpStatus.OK).body(getBalancesResponse);
     }
 
     @Override
     public void rollbackBaseTransactions(TransactionData transactionData) {
-        transactionData.getBaseTransactions().forEach(baseTransactionData ->
-                preBalanceMap.computeIfPresent(baseTransactionData.getAddressHash(), (addressHash, amount) -> amount.add(baseTransactionData.getAmount().negate()))
-        );
+        transactionData.getBaseTransactions().forEach(baseTransactionData -> {
+            Hash currencyHash = getNativeCurrencyHashIfNull(baseTransactionData.getCurrencyHash());
+            preBalanceMap.computeIfPresent(baseTransactionData.getAddressHash(), (addressHash, currencyHashPreBalanceMap) -> {
+                currencyHashPreBalanceMap.computeIfPresent(currencyHash, (currentCurrencyHash, currentAmount) ->
+                        currentAmount.add(baseTransactionData.getAmount().negate())
+                );
+                return currencyHashPreBalanceMap;
+            });
+        });
     }
 
     @Override
     public void validateBalances() {
-        preBalanceMap.forEach((hash, bigDecimal) -> {
-            if (bigDecimal.signum() == -1) {
-                log.error("PreBalance Validation failed!");
-                throw new IllegalArgumentException("ClusterStamp or database are corrupted.");
-            }
+        preBalanceMap.forEach((hash, currencyHashPreBalanceMap) -> {
+            currencyHashPreBalanceMap.forEach((currencyHash, preBalance) -> {
+                if (preBalance.signum() == -1) {
+                    log.error("PreBalance Validation failed!");
+                    throw new IllegalArgumentException("ClusterStamp or database are corrupted.");
+                }
+            });
+
         });
-        balanceMap.forEach((hash, bigDecimal) -> {
-            if (bigDecimal.signum() == -1) {
-                log.error("Balance Validation failed!");
-                throw new IllegalArgumentException("ClusterStamp or database are corrupted.");
-            }
+        balanceMap.forEach((hash, currencyHashBalanceMap) -> {
+            currencyHashBalanceMap.forEach((currencyHash, balance) -> {
+                if (balance.signum() == -1) {
+                    log.error("Balance Validation failed!");
+                    throw new IllegalArgumentException("ClusterStamp or database are corrupted.");
+                }
+            });
         });
         log.info("Balance Validation completed");
     }
 
     @Override
-    public void updateBalanceFromClusterStamp(Hash addressHash, BigDecimal amount) throws IllegalArgumentException {
-        if (balanceMap.containsKey(addressHash)) {
-            log.error("The address {} was already found in the clusterstamp", addressHash);
-            throw new IllegalArgumentException(String.format("The address %s was already found in the clusterstamp", addressHash));
+    public void updateBalanceFromClusterStamp(Hash addressHash, Hash currencyHash, BigDecimal amount) throws IllegalArgumentException {
+        currencyHash = getNativeCurrencyHashIfNull(currencyHash);
+        if (balanceMap.containsKey(addressHash) && balanceMap.get(addressHash).containsKey(currencyHash)) {
+            log.error("The address {} for currency {} was already found in the clusterstamp", addressHash, currencyHash);
+            throw new IllegalArgumentException(String.format("The address %s for currency %s was already found in the clusterstamp", addressHash, currencyHash));
         }
-        balanceMap.put(addressHash, amount);
+        balanceMap.putIfAbsent(addressHash, new ConcurrentHashMap<>());
+        balanceMap.get(addressHash).put(currencyHash, amount);
         log.trace("Loading from clusterstamp into inMem balance+preBalance address {} and amount {}", addressHash, amount);
     }
 
@@ -116,27 +138,47 @@ public class BaseNodeBalanceService implements IBalanceService {
     }
 
     @Override
-    public void updateBalance(Hash addressHash, BigDecimal amount) {
-        balanceMap.computeIfPresent(addressHash, (currentHash, currentAmount) ->
-                currentAmount.add(amount));
-        balanceMap.putIfAbsent(addressHash, amount);
+    public void updateBalance(Hash addressHash, Hash currencyHash, BigDecimal amount) {
+        updateBalance(addressHash, currencyHash, amount, balanceMap);
     }
 
     @Override
-    public void updatePreBalance(Hash addressHash, BigDecimal amount) {
-        preBalanceMap.computeIfPresent(addressHash, (currentHash, currentAmount) ->
-                currentAmount.add(amount));
-        preBalanceMap.putIfAbsent(addressHash, amount);
+    public void updatePreBalance(Hash addressHash, Hash currencyHash, BigDecimal amount) {
+        updateBalance(addressHash, currencyHash, amount, preBalanceMap);
+    }
+
+    private void updateBalance(Hash addressHash, Hash currencyHash, BigDecimal amount, Map<Hash, Map<Hash, BigDecimal>> balanceMap) {
+        final Hash finalCurrencyHash = getNativeCurrencyHashIfNull(currencyHash);
+        balanceMap.computeIfPresent(addressHash, (currentHash, currentCurrencyHashBalanceMap) -> {
+            currentCurrencyHashBalanceMap.computeIfPresent(finalCurrencyHash, (currentCurrencyHash, currentBalance) -> currentBalance.add(amount));
+            currentCurrencyHashBalanceMap.putIfAbsent(finalCurrencyHash, amount);
+            return currentCurrencyHashBalanceMap;
+        });
+        balanceMap.putIfAbsent(addressHash, getInitialCurrencyHashBalanceMap(currencyHash, amount));
+    }
+
+    private Map<Hash, BigDecimal> getInitialCurrencyHashBalanceMap(Hash currencyHash, BigDecimal amount) {
+        Map<Hash, BigDecimal> currencyHashBalanceMap = new ConcurrentHashMap<>();
+        currencyHashBalanceMap.put(currencyHash, amount);
+        return currencyHashBalanceMap;
     }
 
     @Override
-    public BigDecimal getBalanceByAddress(Hash addressHash) {
-        return balanceMap.containsKey(addressHash) ? balanceMap.get(addressHash) : BigDecimal.ZERO;
+    public BigDecimal getBalance(Hash addressHash, Hash currencyHash) {
+        return getBalance(addressHash, currencyHash, balanceMap);
     }
 
     @Override
-    public BigDecimal getPreBalanceByAddress(Hash addressHash) {
-        return preBalanceMap.containsKey(addressHash) ? preBalanceMap.get(addressHash) : BigDecimal.ZERO;
+    public BigDecimal getPreBalance(Hash addressHash, Hash currencyHash) {
+        return getBalance(addressHash, currencyHash, preBalanceMap);
     }
 
+    private BigDecimal getBalance(Hash addressHash, Hash
+            currencyHash, Map<Hash, Map<Hash, BigDecimal>> balanceMap) {
+        return Optional.ofNullable(Optional.ofNullable(balanceMap.get(addressHash)).orElse(new ConcurrentHashMap<>()).get(currencyHash)).orElse(BigDecimal.ZERO);
+    }
+
+    private Hash getNativeCurrencyHashIfNull(Hash currencyHash) {
+        return Optional.ofNullable(currencyHash).orElse(NATIVE_CURRENCY_HASH);
+    }
 }
