26a1b62877277da34738955a661b6c6e6d4bd555
cotitech <eli@coti.io>
2019-09-15 11:44:46 +0300

    native currency balance  websocket notification

--- fullnode/src/main/java/io/coti/fullnode/services/BalanceService.java
+++ fullnode/src/main/java/io/coti/fullnode/services/BalanceService.java
@@ -6,6 +6,8 @@ import io.coti.fullnode.websocket.WebSocketSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static io.coti.basenode.services.CurrencyService.NATIVE_CURRENCY_HASH;
+
 
 @Service
 public class BalanceService extends BaseNodeBalanceService {
@@ -15,6 +17,6 @@ public class BalanceService extends BaseNodeBalanceService {
 
     @Override
     public void continueHandleBalanceChanges(Hash addressHash) {
-        webSocketSender.notifyBalanceChange(addressHash, balanceMap.get(addressHash), preBalanceMap.get(addressHash));
+        webSocketSender.notifyBalanceChange(addressHash, balanceMap.get(addressHash).get(NATIVE_CURRENCY_HASH), preBalanceMap.get(addressHash).get(NATIVE_CURRENCY_HASH));
     }
 }
