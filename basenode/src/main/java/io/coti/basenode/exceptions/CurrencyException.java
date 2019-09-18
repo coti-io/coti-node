f7120e8ec002c9b22f5bf606e1e30320f7413bf2
cotitech <eli@coti.io>
2019-09-18 14:28:37 +0300

    new TransactionType for token generation

--- basenode/src/main/java/io/coti/basenode/exceptions/CurrencyException.java
+++ basenode/src/main/java/io/coti/basenode/exceptions/CurrencyException.java
@@ -1,7 +1,12 @@
 package io.coti.basenode.exceptions;
 
-public class CurrencyException extends RuntimeException {
+public class CurrencyException extends CotiRunTimeException {
 
-    public CurrencyException(String message) { super(message); }
+    public CurrencyException(String message) {
+        super(message);
+    }
 
+    public CurrencyException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
