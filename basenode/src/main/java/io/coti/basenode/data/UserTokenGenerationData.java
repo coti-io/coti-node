b5339b43d0178a9ea7a22477bbb86dfc148b68a3
Alex Stoliar <alex@coti.io>
2019-09-19 10:09:54 +0300

    add token generation transaction handling after propagation

--- basenode/src/main/java/io/coti/basenode/data/UserTokenGenerationData.java
+++ basenode/src/main/java/io/coti/basenode/data/UserTokenGenerationData.java
@@ -11,6 +11,11 @@ public class UserTokenGenerationData implements IEntity {
     private Hash userHash;
     private Map<Hash,Hash> transactionHashToCurrencyHashMap;
 
+    public UserTokenGenerationData(Hash userHash, Map<Hash,Hash> transactionHashToCurrencyHashMap){
+        this.userHash = userHash;
+        this.transactionHashToCurrencyHashMap = transactionHashToCurrencyHashMap;
+    }
+
     @Override
     public Hash getHash() {
         return userHash;
--- basenode/src/main/java/io/coti/basenode/http/GetTokenGenerationDataResponse.java
+++ basenode/src/main/java/io/coti/basenode/http/GetTokenGenerationDataResponse.java
@@ -5,13 +5,37 @@ import io.coti.basenode.data.Hash;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-//TODO 9/17/2019 astolia: move to financial server
 @Data
 public class GetTokenGenerationDataResponse extends BaseResponse {
 
     @NotNull
-    private Map<Hash, CurrencyData> transactionHashToGeneratedCurrency;
+    private Map<Hash, CurrencyData> completedTransactionHashToGeneratedCurrency;
+    @NotNull
+    private Map<Hash, CurrencyData> pendingTransactionHashToGeneratedCurrency;
+    @NotNull
+    private List<Hash> unusedConfirmedTransactions;
+
+    public GetTokenGenerationDataResponse(){
+        completedTransactionHashToGeneratedCurrency = new HashMap<>();
+        pendingTransactionHashToGeneratedCurrency = new HashMap<>();
+        unusedConfirmedTransactions = new ArrayList<>();
+    }
+
+    public void addUnusedConfirmedTransaction(Hash transactionHash){
+        unusedConfirmedTransactions.add(transactionHash);
+    }
+
+    public void addPendingTransactionHashToGeneratedCurrency(Hash transactionHash, CurrencyData currencyData){
+        pendingTransactionHashToGeneratedCurrency.put(transactionHash,currencyData);
+    }
+
+    public void addCompletedTransactionHashToGeneratedCurrency(Hash transactionHash,CurrencyData currencyData){
+        completedTransactionHashToGeneratedCurrency.put(transactionHash, currencyData);
+    }
 
 }
