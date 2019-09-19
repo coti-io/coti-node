f886e342d6bdb3b89c070a5c7eca35db7d9b8ae8
Tomer <tomer@coti.io>
2019-09-19 17:15:15 +0300

    validate currency data

---
+++ basenode/src/main/java/io/coti/basenode/crypto/GenerateTokenRequestCrypto.java
@@ -0,0 +1,21 @@
+package io.coti.basenode.crypto;
+
+import io.coti.basenode.http.GenerateTokenRequest;
+import org.springframework.stereotype.Service;
+
+import java.nio.ByteBuffer;
+
+@Service
+public class GenerateTokenRequestCrypto extends SignatureCrypto<GenerateTokenRequest> {
+    @Override
+    public byte[] getSignatureMessage(GenerateTokenRequest generateTokenRequest) {
+
+        byte[] transactionHashInBytes = generateTokenRequest.getTransactionHash().getBytes();
+        byte[] currencyHashInBytes = generateTokenRequest.getCurrencyData().getHash().getBytes();
+
+        byte[] generateTokenRequestBufferInBytes = ByteBuffer.allocate(transactionHashInBytes.length + currencyHashInBytes.length).
+                put(transactionHashInBytes).put(currencyHashInBytes).array();
+
+        return CryptoHelper.cryptoHash(generateTokenRequestBufferInBytes).getBytes();
+    }
+}
