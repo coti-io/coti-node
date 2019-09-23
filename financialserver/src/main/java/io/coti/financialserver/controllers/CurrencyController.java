a89406df234e20b52682fc2d8e88526175df3b8a
cotitech <eli@coti.io>
2019-09-23 19:08:13 +0300

    generate token code review

---
+++ financialserver/src/main/java/io/coti/financialserver/controllers/CurrencyController.java
@@ -0,0 +1,31 @@
+package io.coti.financialserver.controllers;
+
+import io.coti.basenode.http.interfaces.IResponse;
+import io.coti.financialserver.http.GenerateTokenRequest;
+import io.coti.financialserver.http.GetUserTokensRequest;
+import io.coti.financialserver.services.CurrencyService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequestMapping("/currencies")
+public class CurrencyController {
+
+    @Autowired
+    private CurrencyService currencyService;
+
+    @PostMapping(path = "/token/user")
+    public ResponseEntity<IResponse> getUserTokens(@Valid @RequestBody GetUserTokensRequest getUserTokensRequest) {
+        return currencyService.getUserTokens(getUserTokensRequest);
+    }
+
+    @PutMapping(path = "/token/generate")
+    public ResponseEntity<IResponse> generateToken(GenerateTokenRequest generateTokenRequest) {
+        return currencyService.generateToken(generateTokenRequest);
+    }
+}
--- financialserver/src/main/java/io/coti/financialserver/http/GenerateTokenRequest.java
+++ financialserver/src/main/java/io/coti/financialserver/http/GenerateTokenRequest.java
@@ -1,36 +1,27 @@
-package io.coti.basenode.http;
+package io.coti.financialserver.http;
 
 import io.coti.basenode.data.Hash;
 import io.coti.basenode.data.OriginatorCurrencyData;
 import io.coti.basenode.data.SignatureData;
 import io.coti.basenode.data.interfaces.ISignValidatable;
-import io.coti.basenode.data.interfaces.ISignable;
+import io.coti.basenode.http.Request;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
-public class GenerateTokenRequest extends SerializableRequest implements ISignable, ISignValidatable {
+public class GenerateTokenRequest extends Request implements ISignValidatable {
 
-    private Hash hash;
     @NotEmpty
     private Hash transactionHash;
-
     @NotEmpty
     private OriginatorCurrencyData originatorCurrencyData;
-    @NotNull
-    private Hash signerHash;
     @NotNull
     private SignatureData signature;
 
     @Override
     public Hash getSignerHash() {
         return originatorCurrencyData.getOriginatorHash();
-    }
-
-    @Override
-    public void setSignerHash(Hash signerHash) {
-
     }
 }
