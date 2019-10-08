90ce0a119c5c276021aec31393a383c534878929
cotitech <eli@coti.io>
2019-10-08 13:41:01 +0300

    initiate generated token review

--- dspnode/src/main/java/io/coti/dspnode/services/CurrencyService.java
+++ dspnode/src/main/java/io/coti/dspnode/services/CurrencyService.java
@@ -2,7 +2,7 @@ package io.coti.dspnode.services;
 
 
 import io.coti.basenode.communication.interfaces.IPropagationPublisher;
-import io.coti.basenode.data.CurrencyNoticeData;
+import io.coti.basenode.data.InitiatedTokenNoticeData;
 import io.coti.basenode.data.NodeType;
 import io.coti.basenode.services.BaseNodeCurrencyService;
 import lombok.extern.slf4j.Slf4j;
@@ -19,8 +19,8 @@ public class CurrencyService extends BaseNodeCurrencyService {
     private IPropagationPublisher propagationPublisher;
 
     @Override
-    public void handlePropagatedCurrencyNotice(CurrencyNoticeData currencyNoticeData) {
-        super.handlePropagatedCurrencyNotice(currencyNoticeData);
-        propagationPublisher.propagate(currencyNoticeData, Arrays.asList(NodeType.FullNode));
+    public void handlePropagatedCurrencyNotice(InitiatedTokenNoticeData initiatedTokenNoticeData) {
+        super.handlePropagatedCurrencyNotice(initiatedTokenNoticeData);
+        propagationPublisher.propagate(initiatedTokenNoticeData, Arrays.asList(NodeType.FullNode));
     }
 }
