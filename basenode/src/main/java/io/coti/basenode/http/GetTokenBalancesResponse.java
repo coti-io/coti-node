653518b3a5eab0b5b3a5ea93a27dd582d5c969ae
cotitech <eli@coti.io>
2019-10-27 16:03:18 +0200

    get tokens fix

--- basenode/src/main/java/io/coti/basenode/http/GetTokenBalancesResponse.java
+++ basenode/src/main/java/io/coti/basenode/http/GetTokenBalancesResponse.java
@@ -4,15 +4,14 @@ import io.coti.basenode.data.Hash;
 import io.coti.basenode.http.data.AddressBalance;
 import lombok.Data;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Data
 public class GetTokenBalancesResponse extends BaseResponse {
 
-    private Map<Hash, Map<Hash, AddressBalance>> tokenToAddressesBalance;
+    private Map<Hash, Map<Hash, AddressBalance>> tokenBalances;
 
-    public GetTokenBalancesResponse() {
-        this.tokenToAddressesBalance = new HashMap<>();
+    public GetTokenBalancesResponse(Map<Hash, Map<Hash, AddressBalance>> tokenBalances) {
+        this.tokenBalances = tokenBalances;
     }
 }
