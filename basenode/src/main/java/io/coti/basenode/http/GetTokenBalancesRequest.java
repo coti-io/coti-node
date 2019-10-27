90db6c07805fcb1a88d2ecabca7372ad988266a8
Tomer <tomer@coti.io>
2019-10-27 10:31:35 +0200

    get Tokens WIP
    get tokens balances

---
+++ basenode/src/main/java/io/coti/basenode/http/GetTokenBalancesRequest.java
@@ -0,0 +1,14 @@
+package io.coti.basenode.http;
+
+import io.coti.basenode.data.Hash;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Data
+public class GetTokenBalancesRequest extends Request {
+
+    @NotNull(message = "Addresses must not be blank")
+    public List<Hash> addresses;
+}
