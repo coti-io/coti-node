06b7a651ceab041055d8abe4f8fc8ead66a5543f
cotitech <eli@coti.io>
2019-11-03 20:01:45 +0200

    generate token fee finalized

--- financialserver/src/main/java/io/coti/financialserver/http/GetUserTokensRequest.java
+++ financialserver/src/main/java/io/coti/financialserver/http/GetUserTokensRequest.java
@@ -7,7 +7,6 @@ import io.coti.basenode.http.Request;
 import lombok.Data;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.Instant;
 
--- nodemanager/src/main/java/io/coti/nodemanager/services/HealthCheckService.java
+++ nodemanager/src/main/java/io/coti/nodemanager/services/HealthCheckService.java
@@ -2,7 +2,6 @@ package io.coti.nodemanager.services;
 
 import io.coti.basenode.data.Hash;
 import io.coti.basenode.data.NetworkNodeData;
-import io.coti.basenode.data.NodeType;
 import io.coti.basenode.services.interfaces.INetworkService;
 import io.coti.nodemanager.data.NetworkNodeStatus;
 import io.coti.nodemanager.model.ActiveNodes;
