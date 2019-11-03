2bea7c7649962f09d738fbca9673e73b9760b6e9
Anton <anton@coti.io>
2019-11-03 16:20:55 +0200

    currency hash in fee BTs

--- fullnode/src/main/java/io/coti/fullnode/http/data/FullNodeFeeResponseData.java
+++ fullnode/src/main/java/io/coti/fullnode/http/data/FullNodeFeeResponseData.java
@@ -15,6 +15,7 @@ public class FullNodeFeeResponseData implements IResponseData {
     private String amount;
     private String originalAmount;
     private String addressHash;
+    private String currencyHash;
     private Instant createTime;
     private String name;
     private SignatureData signatureData;
@@ -24,6 +25,7 @@ public class FullNodeFeeResponseData implements IResponseData {
         this.amount = fullNodeFeeData.getAmount().toPlainString();
         this.originalAmount = fullNodeFeeData.getOriginalAmount().toPlainString();
         this.addressHash = fullNodeFeeData.getAddressHash().toString();
+        this.currencyHash = fullNodeFeeData.getCurrencyHash().toString();
         this.createTime = fullNodeFeeData.getCreateTime();
         this.name = BaseTransactionName.getName(FullNodeFeeData.class).name();
         this.signatureData = fullNodeFeeData.getSignatureData();
