c115b83eb7ea2ef7340938ee980f177643770ddc
Anton <anton@coti.io>
2019-11-05 14:38:40 +0200

    calculateTokenGenerationFee + validateBaseTransactionAmounts + optimization

--- basenode/src/main/java/io/coti/basenode/services/interfaces/ITransactionTypeValidation.java
+++ basenode/src/main/java/io/coti/basenode/services/interfaces/ITransactionTypeValidation.java
@@ -1,16 +1,17 @@
 package io.coti.basenode.services.interfaces;
 
+import io.coti.basenode.data.Hash;
 import io.coti.basenode.data.OutputBaseTransactionData;
 import io.coti.basenode.data.TransactionData;
 
 import java.util.List;
 
 public interface ITransactionTypeValidation {
-    boolean validateBaseTransactions(TransactionData transactionData);
+    boolean validateBaseTransactions(TransactionData transactionData, Hash nativeCurrencyHash);
 
     boolean validateInputBaseTransactions(TransactionData transactionData);
 
-    boolean validateOutputBaseTransactions(TransactionData transactionData, boolean skipValidationOfReducedAmount);
+    boolean validateOutputBaseTransactions(TransactionData transactionData, boolean skipValidationOfReducedAmount, Hash nativeCurrencyHash);
 
     boolean validateReducedAmount(List<OutputBaseTransactionData> outputBaseTransactions);
 }
