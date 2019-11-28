06de0f32d8497cf5bf255ffdf195c37d8ac65f1c
cotitech <eli@coti.io>
2019-11-28 19:02:12 +0200

    minting token review

--- basenode/src/main/java/io/coti/basenode/data/TokenMintingData.java
+++ basenode/src/main/java/io/coti/basenode/data/TokenMintingData.java
@@ -1,8 +1,10 @@
 package io.coti.basenode.data;
 
-import io.coti.basenode.data.interfaces.IServiceDataInBaseTransaction;
+import io.coti.basenode.data.interfaces.ISignValidatable;
+import io.coti.basenode.data.interfaces.ITokenServiceData;
 import lombok.Data;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.nio.ByteBuffer;
@@ -10,27 +12,24 @@ import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 
 @Data
-public class TokenMintingFeeDataInBaseTransaction implements IServiceDataInBaseTransaction {
+public class TokenMintingData implements ITokenServiceData, ISignValidatable {
 
     @NotNull
     private Hash mintingCurrencyHash;
     @NotNull
     private BigDecimal mintingAmount;
-    private Hash receiverAddress;
     @NotNull
-    private Instant requestTime;
+    private @Valid Hash receiverAddress;
     @NotNull
-    private BigDecimal feeQuote;
+    private Instant createTime;
+    @NotNull
+    private BigDecimal feeAmount;
+    @NotNull
+    private @Valid Hash signerHash;
+    @NotNull
+    private @Valid SignatureData signature;
 
-    public TokenMintingFeeDataInBaseTransaction() {
-    }
-
-    public TokenMintingFeeDataInBaseTransaction(Hash mintingCurrencyHash, BigDecimal mintingAmount, Hash receiverAddress, Instant requestTime, BigDecimal feeQuote) {
-        this.mintingCurrencyHash = mintingCurrencyHash;
-        this.mintingAmount = mintingAmount;
-        this.receiverAddress = receiverAddress;
-        this.requestTime = requestTime;
-        this.feeQuote = feeQuote;
+    private TokenMintingData() {
     }
 
     @Override
@@ -38,11 +37,11 @@ public class TokenMintingFeeDataInBaseTransaction implements IServiceDataInBaseT
         byte[] bytesOfCurrencyHash = mintingCurrencyHash.getBytes();
         byte[] bytesOfAmount = mintingAmount.stripTrailingZeros().toPlainString().getBytes(StandardCharsets.UTF_8);
         byte[] bytesOfReceiverAddress = receiverAddress.getBytes();
-        byte[] bytesOfFeeQuote = feeQuote.stripTrailingZeros().toPlainString().getBytes(StandardCharsets.UTF_8);
+        byte[] bytesOfFeeQuote = feeAmount.stripTrailingZeros().toPlainString().getBytes(StandardCharsets.UTF_8);
 
         return ByteBuffer.allocate(bytesOfCurrencyHash.length + bytesOfAmount.length
                 + bytesOfReceiverAddress.length + Long.BYTES + bytesOfFeeQuote.length)
                 .put(bytesOfCurrencyHash).put(bytesOfAmount).put(bytesOfReceiverAddress)
-                .putLong(requestTime.toEpochMilli()).put(bytesOfFeeQuote).array();
+                .putLong(createTime.toEpochMilli()).put(bytesOfFeeQuote).array();
     }
 }
