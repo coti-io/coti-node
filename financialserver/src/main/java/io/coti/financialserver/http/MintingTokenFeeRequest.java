c67a71786f9b70da9cd867bb7d0f9ffe98e3ba68
Anton <anton@coti.io>
2019-12-01 17:03:43 +0200

    minor fixes

--- financialserver/src/main/java/io/coti/financialserver/http/MintingTokenFeeRequest.java
+++ financialserver/src/main/java/io/coti/financialserver/http/MintingTokenFeeRequest.java
@@ -1,12 +1,9 @@
 package io.coti.financialserver.http;
 
 import io.coti.basenode.data.Hash;
-import io.coti.basenode.data.SignatureData;
 import io.coti.basenode.data.TokenMintingData;
-import io.coti.basenode.data.interfaces.ISignValidatable;
 import io.coti.basenode.http.Request;
 import io.coti.financialserver.data.MintingFeeQuoteData;
-import io.coti.financialserver.http.data.MintingFeeData;
 import lombok.Data;
 
 import javax.validation.Valid;
@@ -15,10 +12,6 @@ import javax.validation.constraints.NotNull;
 @Data
 public class MintingTokenFeeRequest extends Request {
 
-    @NotNull
-    private @Valid MintingFeeData mintingFeeData;
-    @NotNull
-    private @Valid Hash userHash;
     @NotNull
     private @Valid TokenMintingData tokenMintingData;
     private @Valid MintingFeeQuoteData mintingFeeQuoteData;
