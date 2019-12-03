20097e37b6074ce1dbca28736df9c483b086efb1
cotitech <eli@coti.io>
2019-12-03 18:19:22 +0200

    minting token review 2

--- financialserver/src/main/java/io/coti/financialserver/crypto/TokenMintingCrypto.java
+++ financialserver/src/main/java/io/coti/financialserver/crypto/TokenMintingCrypto.java
@@ -3,11 +3,7 @@ package io.coti.financialserver.crypto;
 import io.coti.basenode.crypto.CryptoHelper;
 import io.coti.basenode.crypto.SignatureValidationCrypto;
 import io.coti.basenode.data.TokenMintingData;
-import io.coti.financialserver.http.MintingTokenFeeRequest;
 import org.springframework.stereotype.Service;
-
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 
 @Service
 public class TokenMintingCrypto extends SignatureValidationCrypto<TokenMintingData> {
