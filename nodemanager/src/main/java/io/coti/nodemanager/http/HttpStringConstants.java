794636db525b7e7ea060d429bda0a2624bf0c4c1
Tomer <tomer@coti.io>
2020-02-04 12:50:25 +0200

    node manager add event refactor wip

--- nodemanager/src/main/java/io/coti/nodemanager/http/HttpStringConstants.java
+++ nodemanager/src/main/java/io/coti/nodemanager/http/HttpStringConstants.java
@@ -11,6 +11,7 @@ public class HttpStringConstants extends BaseNodeHttpStringConstants {
     public static final String ADDING_EVENT_PAIR_INCORRECT_TIME = "Can Not Add FullNode Beginning Event Because The Time Given Is After Existing Event";
     public static final String NODE_HISTORY_RECORD_HAS_BEEN_ADDED_MANUALLY = "A History Record for Node %s Has Been Added Manually";
     public static final String ADDING_EVENT_INCORRECT_FUTURE_TIME = "Can not add future event";
+    public static final String ADDING_EVENT_NONACTIVATED_NODE_HASH = "Can not add event to a non activated node";
     public static final String ADDING_EVENT_INCORRECT_INACTIVE_STATUS = "Can not add an Inactive event not part of a pair";
     public static final String ADDING_EVENT_CONSECUTIVE_SAME_STATUS = "Can not add a consecutive identical status";
 }
