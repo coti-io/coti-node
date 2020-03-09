537ae13a74b3ea35180f54ce8813b893c63d6f47
Tomer <tomer@coti.io>
2020-03-09 20:23:48 +0200

    impl ientity

--- dspnode/src/main/java/io/coti/dspnode/data/UnconfirmedReceivedTransactionHashDspData.java
+++ dspnode/src/main/java/io/coti/dspnode/data/UnconfirmedReceivedTransactionHashDspData.java
@@ -2,10 +2,11 @@ package io.coti.dspnode.data;
 
 import io.coti.basenode.data.Hash;
 import io.coti.basenode.data.UnconfirmedReceivedTransactionHashData;
+import io.coti.basenode.data.interfaces.IEntity;
 import lombok.Data;
 
 @Data
-public class UnconfirmedReceivedTransactionHashDspData extends UnconfirmedReceivedTransactionHashData {
+public class UnconfirmedReceivedTransactionHashDspData extends UnconfirmedReceivedTransactionHashData implements IEntity {
 
     private static final long serialVersionUID = -3173653431370228166L;
     private boolean dSPVoteOnly;
