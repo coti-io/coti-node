1fc037489248b0c3d5bad83a44d5db57faba1fa4
cotitech <eli@coti.io>
2020-04-05 13:52:48 +0300

    save

--- basenode/src/main/java/io/coti/basenode/crypto/CurrencyTypeRegistrationCrypto.java
+++ basenode/src/main/java/io/coti/basenode/crypto/CurrencyTypeRegistrationCrypto.java
@@ -3,11 +3,23 @@ package io.coti.basenode.crypto;
 import io.coti.basenode.data.CurrencyTypeRegistrationData;
 import org.springframework.stereotype.Service;
 
+import java.nio.ByteBuffer;
+
 @Service
 public class CurrencyTypeRegistrationCrypto extends SignatureCrypto<CurrencyTypeRegistrationData> {
 
     @Override
     public byte[] getSignatureMessage(CurrencyTypeRegistrationData currencyTypeRegistrationData) {
-        return CryptoHelper.cryptoHash(currencyTypeRegistrationData.getMessageInBytes()).getBytes();
+        byte[] symbolInBytes = currencyTypeRegistrationData.getSymbol().getBytes();
+        byte[] currencyTypeInBytes = currencyTypeRegistrationData.getCurrencyType().name().getBytes();
+        byte[] bytesOfCurrencyRateSourceType = currencyTypeRegistrationData.getCurrencyRateSourceType() == null ? new byte[0] : currencyTypeRegistrationData.getCurrencyRateSourceType().name().getBytes();
+        byte[] bytesOfRateSource = currencyTypeRegistrationData.getRateSource() == null ? new byte[0] : currencyTypeRegistrationData.getRateSource().getBytes();
+        byte[] bytesOfProtectionModel = currencyTypeRegistrationData.getProtectionModel() == null ? new byte[0] : currencyTypeRegistrationData.getProtectionModel().getBytes();
+        byte[] createTimeInBytes = ByteBuffer.allocate(Long.BYTES).putLong(currencyTypeRegistrationData.getCreateTime().toEpochMilli()).array();
+
+        ByteBuffer currencyTypeRegistrationBuffer = ByteBuffer.allocate(symbolInBytes.length + currencyTypeInBytes.length
+                + bytesOfCurrencyRateSourceType.length + bytesOfRateSource.length + bytesOfProtectionModel.length + createTimeInBytes.length)
+                .put(symbolInBytes).put(currencyTypeInBytes).put(bytesOfCurrencyRateSourceType).put(bytesOfRateSource).put(bytesOfProtectionModel).put(createTimeInBytes);
+        return CryptoHelper.cryptoHash(currencyTypeRegistrationBuffer.array()).getBytes();
     }
 }
