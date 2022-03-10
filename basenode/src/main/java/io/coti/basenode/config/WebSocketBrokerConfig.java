65b98edc4b13979bd451e44fa66b7a985634b3a1
cotitech <eli@coti.io>
2022-03-10 17:50:05 +0200

    setting ws send buffer size to 5120 kb

--- basenode/src/main/java/io/coti/basenode/config/WebSocketBrokerConfig.java
+++ basenode/src/main/java/io/coti/basenode/config/WebSocketBrokerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -19,5 +20,10 @@ public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+        registry.setSendBufferSizeLimit(5120 * 1024);
     }
 }
