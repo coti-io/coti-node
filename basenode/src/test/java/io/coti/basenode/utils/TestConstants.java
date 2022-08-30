5e26db5bfc549c4c38e7e95d0d65774c388dca1b
Tomer <tomer@coti.io>
2022-08-30 09:43:33 +0300

    Trust score consensus unit-tests

--- basenode/src/test/java/io/coti/basenode/utils/TestConstants.java
+++ basenode/src/test/java/io/coti/basenode/utils/TestConstants.java
@@ -1,8 +1,12 @@
 package io.coti.basenode.utils;
 
 public final class TestConstants {
-
     public static final String[] hexaOptions = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
     public static final int SIZE_OF_HASH = 64;
+    public static final int SIZE_OF_BASE_TRANSACTION_HASH = 136;
+    public static final int SIZE_OF_ADDRESS_HASH = 136;
+    public static final String TRANSACTION_DESCRIPTION = "test";
+    public static final int MAX_TRUST_SCORE = 100;
+    public static final String NATIVE_CURRENCY_HASH = "e72d2137d5cfcc672ab743bddbdedb4e059ca9d3db3219f4eb623b01";
 
 }
