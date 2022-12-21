f2c7fcc5cb7a793c6b05bafd62d02516fddaccf3
Guy Mesika <guy@coti.io>
2022-12-22 01:42:14 +0200

    health indicators refactoring

---
+++ basenode/src/main/java/io/coti/basenode/data/HealthMetricOutput.java
@@ -0,0 +1,18 @@
+package io.coti.basenode.data;
+
+import lombok.Data;
+
+@Data
+public class HealthMetricOutput {
+
+    private HealthMetricOutputType type;
+    private String label;
+    private Long value;
+
+    public HealthMetricOutput (HealthMetricOutputType type, String label, long value) {
+        this.type = type;
+        this.label = label;
+        this.value = value;
+    }
+
+}
