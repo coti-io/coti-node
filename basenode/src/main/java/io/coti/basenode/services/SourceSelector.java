08d6fe54be18de9b4d594e750d9346a6e54eccc1
Guy Mesika <guy@coti.io>
2023-03-06 22:03:29 +0200

    dont clone entire object - only attachment time used

--- basenode/src/main/java/io/coti/basenode/services/SourceSelector.java
+++ basenode/src/main/java/io/coti/basenode/services/SourceSelector.java
@@ -53,14 +53,14 @@ public class SourceSelector implements ISourceSelector {
                 int lowIndex = roundedTrustScore - 1;
                 int highIndex = roundedTrustScore + 1;
 
-                neighbourSources.addAll(trustScoreToSourceListMapping.get(roundedTrustScore).stream().map(hash -> SerializationUtils.clone(sourceMap.get(hash))).collect(toList()));
+                neighbourSources.addAll(trustScoreToSourceListMapping.get(roundedTrustScore).stream().map(sourceMap::get).collect(toList()));
 
                 for (int trustScoreDifference = 0; trustScoreDifference < maxNeighbourhoodRadius; trustScoreDifference++) {
                     if (lowIndex >= 0) {
-                        neighbourSources.addAll(trustScoreToSourceListMapping.get(lowIndex).stream().map(hash -> SerializationUtils.clone(sourceMap.get(hash))).collect(toList()));
+                        neighbourSources.addAll(trustScoreToSourceListMapping.get(lowIndex).stream().map(sourceMap::get).collect(toList()));
                     }
                     if (highIndex <= 100) {
-                        neighbourSources.addAll(trustScoreToSourceListMapping.get(highIndex).stream().map(hash -> SerializationUtils.clone(sourceMap.get(hash))).collect(toList()));
+                        neighbourSources.addAll(trustScoreToSourceListMapping.get(highIndex).stream().map(sourceMap::get).collect(toList()));
                     }
                     if ((double) neighbourSources.size() / numberOfSources > (double) minSourcePercentage / 100) {
                         break;
