6e9178c1e593fe01bd939b1662257ede187a6b8e
Guy Mesika <guy@coti.io>
2023-03-12 23:40:49 +0200

    handle failure of validation post waiting for tasks submission
    handle failure of validation post waiting for tasks submission

--- basenode/src/main/java/io/coti/basenode/data/ExecutorData.java
+++ basenode/src/main/java/io/coti/basenode/data/ExecutorData.java
@@ -13,7 +13,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.locks.LockSupport;
 
 @Slf4j
 @Data
@@ -22,68 +21,103 @@ public class ExecutorData {
     private final ExecutorService executorService;
     private final List<Future<?>> futures;
     private final InitializationTransactionHandlerType initializationTransactionHandlerType;
-    private AtomicLong futuresToComplete;
-    private AtomicLong failedFutures;
+    private AtomicLong numberSubmittedTasks;
+    private AtomicLong numberFailedTasks;
+    private AtomicInteger numberCompletedTasks;
 
     public ExecutorData(InitializationTransactionHandlerType initializationTransactionHandlerType) {
         executorService = Executors.newFixedThreadPool(1);
         futures = Collections.synchronizedList(new ArrayList<>());
         this.initializationTransactionHandlerType = initializationTransactionHandlerType;
-        futuresToComplete = new AtomicLong(0);
-        failedFutures = new AtomicLong(0);
+        numberSubmittedTasks = new AtomicLong(0);
+        numberFailedTasks = new AtomicLong(0);
+        numberCompletedTasks = new AtomicInteger(0);
+    }
+
+    public void skipTask() {
+        numberSubmittedTasks.incrementAndGet();
+        numberCompletedTasks.incrementAndGet();
     }
 
     public void submit(Runnable runnable) {
         try {
-            futuresToComplete.incrementAndGet();
             futures.add(executorService.submit(runnable));
+            numberSubmittedTasks.incrementAndGet();
         } catch (Exception exception) {
             log.error("Error while running " + initializationTransactionHandlerType + " executor.\n", exception);
-            failedFutures.incrementAndGet();
+            numberFailedTasks.incrementAndGet();
         }
     }
 
-    public void waitForTermination(AtomicLong transactionsToProcess) {
-        AtomicInteger completedFutures = new AtomicInteger(0);
-        AtomicLong submittedFutures = new AtomicLong(futuresToComplete.get());
-        AtomicInteger checkCounter = new AtomicInteger(0);
-        Thread monitorCompletionThread = getMonitorCompletionThread(completedFutures, transactionsToProcess);
-        monitorCompletionThread.start();
-        while (waitForSubmitCompletion(transactionsToProcess, submittedFutures, checkCounter)) {
-            LockSupport.parkNanos(500_000_000);
+    private boolean waitForSubmitCompletion(AtomicLong completeNumberOfTransactions, AtomicLong lastNumberSubmittedTasks, AtomicInteger numberTimesNoProgress) {
+        if (lastNumberSubmittedTasks.get() < this.numberSubmittedTasks.get()) {
+            lastNumberSubmittedTasks.set(this.numberSubmittedTasks.get());
+            numberTimesNoProgress.set(0);
+            return true;
+        } else {
+            numberTimesNoProgress.incrementAndGet();
         }
+        return completeNumberOfTransactions.get() != this.numberSubmittedTasks.get() && numberFailedTasks.get() == 0 && numberTimesNoProgress.get() < 10;
+    }
+
+    private void validateFuturesSubmitCompleted(AtomicLong completeNumberOfTransactions) {
+        AtomicInteger numberTimesNoProgress = new AtomicInteger(0);
+        AtomicLong lastNumberSubmittedTasks = new AtomicLong(this.numberSubmittedTasks.get());
+        while (waitForSubmitCompletion(completeNumberOfTransactions, lastNumberSubmittedTasks, numberTimesNoProgress)) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new TransactionSyncException(String.format("Error while sleep waiting for submission to complete, error: %s", e));
+            }
+        }
+        if (numberFailedTasks.get() > 0) {
+            throw new TransactionSyncException(String.format("There are %s transactions that got exception while processing by %s handler.", numberFailedTasks.get(), initializationTransactionHandlerType));
+        }
+        if (completeNumberOfTransactions.get() != this.numberSubmittedTasks.get()) {
+            throw new TransactionSyncException(String.format("There is a critical difference between number of transactions (%s) to number of tasks launched to handle (%s)", completeNumberOfTransactions.get(), this.numberSubmittedTasks.get()));
+        }
+    }
+
+    private void validateFuturesCompleted(AtomicInteger numberCompletedTasks) {
         for (Future<?> future : futures) {
             try {
                 future.get();
-                completedFutures.incrementAndGet();
+                numberCompletedTasks.incrementAndGet();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             } catch (ExecutionException e) {
                 log.error("Error while waiting for futures completion for " + initializationTransactionHandlerType + " executor.\n", e);
-                failedFutures.incrementAndGet();
+                numberFailedTasks.incrementAndGet();
             }
         }
-        executorService.shutdown();
-        monitorCompletionThread.interrupt();
-        try {
-            monitorCompletionThread.join();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        if (numberFailedTasks.get() > 0) {
+            throw new TransactionSyncException(String.format("There are %s transactions that got exception while processing by %s handler.", numberFailedTasks.get(), initializationTransactionHandlerType));
         }
-        if (failedFutures.get() > 0) {
-            throw new TransactionSyncException("There are " + failedFutures.get() + " transactions that got exception while processing by " + initializationTransactionHandlerType + " handler.");
+        if (numberCompletedTasks.get() != this.numberSubmittedTasks.get()) {
+            throw new TransactionSyncException(String.format("There is a critical difference between number of launched tasks (%s) to number of successfully processed transactions (%s)", numberSubmittedTasks.get(), numberCompletedTasks.get()));
         }
     }
 
-    private boolean waitForSubmitCompletion(AtomicLong transactionsToProcess, AtomicLong submittedFutures, AtomicInteger checkCounter) {
-        if (submittedFutures.get() < futuresToComplete.get()) {
-            submittedFutures.set(futuresToComplete.get());
-            checkCounter.set(0);
-            return true;
-        } else {
-            checkCounter.incrementAndGet();
+    public void waitForTermination(AtomicLong completeNumberOfTransactions) {
+        Thread monitorCompletionThread = getMonitorCompletionThread(numberCompletedTasks, completeNumberOfTransactions);
+        monitorCompletionThread.start();
+
+        try {
+            validateFuturesSubmitCompleted(completeNumberOfTransactions);
+            validateFuturesCompleted(numberCompletedTasks);
+        } catch (TransactionSyncException e) {
+            log.error(e.toString());
+            throw e;
+        } finally {
+            executorService.shutdown();
+            monitorCompletionThread.interrupt();
+            try {
+                monitorCompletionThread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
-        return transactionsToProcess.get() != futuresToComplete.get() && failedFutures.get() == 0 && checkCounter.get() < 10;
     }
 
     private Thread getMonitorCompletionThread(AtomicInteger completedFutures, AtomicLong transactionsToProcess) {
